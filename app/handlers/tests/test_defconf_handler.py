1ba80e24fce9b722a3f919200f66e81f14fb17e0
Milo Casagrande <milo@ubuntu.com>
2014-03-25 15:54:49 +0100

    Add handlers unittest.

---
+++ app/handlers/tests/test_defconf_handler.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2014 Linaro Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import mongomock
+import unittest
+
+from bson.json_util import dumps
+from mock import (
+    MagicMock,
+    patch,
+)
+
+from handlers import DefConfHandler
+from models import (
+    DB_NAME,
+    DEFCONFIG_COLLECTION
+)
+
+
+class TestDefConfHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.application = MagicMock()
+        self.request = MagicMock()
+
+        self.handler = DefConfHandler(self.application, self.request)
+        self.handler.write = MagicMock()
+
+        super(TestDefConfHandler, self).setUp()
+
+    @patch("handlers.DefConfHandler.db")
+    @patch("handlers.DefConfHandler.collection")
+    def test_job_handler_get_multiple_result(self, mock_coll, mock_db):
+        expcted = [
+            {"_id": "defconf1", "job_id": "job1"},
+            {"_id": "defconf0", "job_id": "job0"}
+        ]
+
+        self.handler.db = mongomock.Database(mongomock.Connection(), DB_NAME)
+        self.handler.collection = mongomock.Collection(
+            self.handler.db, DEFCONFIG_COLLECTION)
+
+        self.handler.collection.insert(dict(_id="defconf0", job_id="job0"))
+        self.handler.collection.insert(dict(_id="defconf1", job_id="job1"))
+
+        self.handler.get()
+        args, _ = self.handler.write.call_args
+        self.assertEqual(args[0], dumps(expcted))
+
+    @patch("handlers.DefConfHandler.db")
+    @patch("handlers.DefConfHandler.collection")
+    def test_job_handler_get_one_result(self, mock_coll, mock_db):
+        expected = {"_id": "defconf2", "job_id": "job2"}
+
+        self.handler.db = mongomock.Database(mongomock.Connection(), DB_NAME)
+        self.handler.collection = mongomock.Collection(
+            self.handler.db, DEFCONFIG_COLLECTION)
+
+        self.handler.collection.insert(dict(_id="defconf0", job_id="job0"))
+        self.handler.collection.insert(dict(_id="defconf1", job_id="job1"))
+        self.handler.collection.insert(dict(_id="defconf2", job_id="job2"))
+        self.handler.collection.insert(dict(_id="defconf3", job_id="job3"))
+
+        kwargs = {"id": "defconf2"}
+
+        self.handler.get(**kwargs)
+        args, _ = self.handler.write.call_args
+        self.assertEqual(args[0], dumps(expected))
---
+++ app/handlers/tests/test_job_handler.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2014 Linaro Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import mongomock
+import unittest
+
+from bson.json_util import dumps
+from mock import (
+    MagicMock,
+    patch,
+)
+
+from handlers import JobHandler
+from models import (
+    DB_NAME,
+    JOB_COLLECTION
+)
+
+
+class TestJobHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.application = MagicMock()
+        self.request = MagicMock()
+
+        self.handler = JobHandler(self.application, self.request)
+        self.handler.write = MagicMock()
+
+        super(TestJobHandler, self).setUp()
+
+    @patch("handlers.JobHandler.db")
+    @patch("handlers.JobHandler.collection")
+    def test_job_handler_get_multiple_result(self, mock_coll, mock_db):
+        expcted = [
+            {"_id": "job1"},
+            {"_id": "job0"}
+        ]
+
+        self.handler.db = mongomock.Database(mongomock.Connection(), DB_NAME)
+        self.handler.collection = mongomock.Collection(
+            self.handler.db, JOB_COLLECTION)
+
+        self.handler.collection.insert(dict(_id="job0"))
+        self.handler.collection.insert(dict(_id="job1"))
+
+        self.handler.get()
+        args, _ = self.handler.write.call_args
+        self.assertEqual(args[0], dumps(expcted))
+
+    @patch("handlers.JobHandler.db")
+    @patch("handlers.JobHandler.collection")
+    def test_job_handler_get_one_result(self, mock_coll, mock_db):
+        expected = {"_id": "job2"}
+
+        self.handler.db = mongomock.Database(mongomock.Connection(), DB_NAME)
+        self.handler.collection = mongomock.Collection(
+            self.handler.db, JOB_COLLECTION)
+
+        self.handler.collection.insert(dict(_id="job0"))
+        self.handler.collection.insert(dict(_id="job1"))
+        self.handler.collection.insert(dict(_id="job2"))
+        self.handler.collection.insert(dict(_id="job3"))
+
+        kwargs = {"id": "job2"}
+
+        self.handler.get(**kwargs)
+        args, _ = self.handler.write.call_args
+        self.assertEqual(args[0], dumps(expected))
