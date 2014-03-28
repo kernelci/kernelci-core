c70b761796bb9e572b8aabe9d6adf3dc1dc457c5
Milo Casagrande <milo@ubuntu.com>
2014-03-28 16:51:53 +0100

    Pylint fixes.

--- app/utils/subscribe.py
+++ app/utils/subscribe.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Collection of utilities to handle subscriptions."""
+
 import types
 
 from models import ID_KEY
