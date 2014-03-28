92e9a33ad6a22c73a6b079d10aa6c28da2b2cc29
Milo Casagrande <milo@ubuntu.com>
2014-03-28 17:21:19 +0100

    Add new unittests.

---
+++ app/utils/tests/test_subscribe.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2014 Linaro Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import mongomock
+import unittest
+
+from mock import patch
+
+from models import DB_NAME
+from utils.subscribe import subscribe_email
+
+
+class TestSubscription(unittest.TestCase):
+
+    def setUp(self):
+        self.database = mongomock.Database(mongomock.Connection(), DB_NAME)
+
+    @patch("utils.subscribe.find_one")
+    def test_subscribe_email_no_job(self, mock_find_one):
+
+        def callback(result):
+            self.assertEquals(result, 404)
+
+        mock_find_one.side_effect = [None]
+        json_obj = dict(
+            job='job',
+            email='email'
+        )
+
+        subscribe_email(json_obj, self.database, callback)
+
+    @patch("utils.subscribe.find_one")
+    def test_subscribe_email_valid_job(self, mock_find_one):
+
+        def callback(result):
+            self.assertEquals(result, 200)
+
+        mock_find_one.side_effect = [
+            dict(_id='job'),
+            None,
+        ]
+        json_obj = dict(
+            job='job',
+            email='email'
+        )
+
+        subscribe_email(json_obj, self.database, callback)
