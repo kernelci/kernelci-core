c5027a47df867a3e31659831c18650c6f03e8044
Milo Casagrande <milo.casagrande@linaro.org>
2014-04-09 15:00:59 +0200

    Add unittest.TestSuite definition.

---
+++ app/tests/__init__.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2014 Linaro Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests suite for kernel-ci-backend."""
+
+import unittest
+
+
+def test_modules():
+    return [
+        'handlers.tests.test_defconf_handler',
+        'handlers.tests.test_job_handler',
+        'handlers.tests.test_subscription_handler',
+        'models.tests.test_models',
+        'utils.tests.test_docimport',
+        'utils.tests.test_subscribe',
+        'utils.tests.test_validator'
+    ]
+
+
+def test_suite():
+    """Create a unittest.TestSuite object."""
+    modules = test_modules()
+    suite = unittest.TestSuite()
+    test_loader = unittest.TestLoader()
+
+    for name in modules:
+        unit_suite = test_loader.loadTestsFromName(name)
+        suite.addTests(unit_suite)
+    return suite
