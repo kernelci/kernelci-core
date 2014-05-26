704100a6609debfde36b65af9aef841c1cac12c9
Milo Casagrande <milo.casagrande@linaro.org>
2014-05-26 18:25:00 +0200

    Fix boot import and parsing.
      * Default only to the JSON format for boot parsing.
    
    Change-Id: Ic150dfe69509285d4a14ec6019d038584b4c323d

--- app/utils/bootimport.py
+++ app/utils/bootimport.py
@@ -16,9 +16,9 @@
 """Container for all the boot import related functions."""
 
 import glob
+import json
 import os
 import pymongo
-import re
 
 from bson import tz_util
 from datetime import (
@@ -26,25 +26,33 @@ from datetime import (
     timedelta
 )
 
-from models import DB_NAME
+from models import (
+    BOOT_LOG_KEY,
+    DB_NAME,
+    DTB_ADDR_KEY,
+    DTB_KEY,
+    INITRD_ADDR_KEY,
+    JOB_KEY,
+    KERNEL_IMAGE_KEY,
+    KERNEL_KEY,
+    UNKNOWN_STATUS,
+)
 from models.boot import BootDocument
 from utils import (
     BASE_PATH,
     LOG,
+    is_hidden,
 )
 from utils.db import save
 
-BOOT_REPORT_SUFFIX = '-boot-report.log'
 # Pattern used for glob matching files on the filesystem.
-BOOT_REPORT_PATTERN = '*' + BOOT_REPORT_SUFFIX
+BOOT_REPORT_PATTERN = 'boot-*.json'
 
-RE_TREE_BRANCH = re.compile(r'^Tree/Branch')
-RE_GIT_DESCRIBE = re.compile(r'^Git\s{1}describe')
-RE_REPORT_SECTION = re.compile(r'^Full\s{1}Report')
-RE_DECFONFIG_SECTION = re.compile(r'.*_defconfig$')
-RE_FAILED_BOOT_SECTION = re.compile(r'^Failed\s{1}boot\s{1}tests')
-RE_CONSOLE_LOG_SECTION = re.compile(r'^Console\s{1}logs\s{1}for\s{1}failures')
-RE_FAILED_LOG_SECTION = re.compile(r'(.*):\s{1}FAIL:')
+# Keys defined only for the boot report JSON format. We store them differently.
+BOOT_TIME_JSON = 'boot_time'
+LOAD_ADDR_JSON = 'loadaddr'
+BOOT_RESULT_JSON = 'boot_result'
+BOOT_WARNINGS_JSON = 'boot_warnings'
 
 
 def import_and_save_boot(json_obj, base_path=BASE_PATH):
@@ -83,206 +91,102 @@ def parse_boot_from_json(json_obj, base_path=BASE_PATH):
         file. It defaults to: /var/www/images/kernel-ci.
     :return A list with all the `BootDocument`s.
     """
+    job = json_obj[JOB_KEY]
+    kernel = json_obj[KERNEL_KEY]
+
+    return _parse_boot(job, kernel, base_path)
+
+
+def _parse_boot(job, kernel, base_path=BASE_PATH):
+    """Traverse the kernel directory and look for boot report logs.
+
+    :param job: The name of the job.
+    :param kernel: The name of the kernel.
+    :param base_path: The base path where to start traversing.
+    :return A list of documents to be saved, or an empty list.
+    """
     docs = []
 
-    job_dir = json_obj['job']
-    kernel_dir = json_obj['kernel']
+    job_dir = os.path.join(base_path, job)
 
-    boot_log_name = kernel_dir + BOOT_REPORT_SUFFIX
-    boot_log = os.path.join(base_path, job_dir, boot_log_name)
+    if not is_hidden(job) and os.path.isdir(job_dir):
+        kernel_dir = os.path.join(job_dir, kernel)
 
-    if os.path.isfile(boot_log):
-        LOG.info("Parsing boot log for %s - %s", job_dir, kernel_dir)
-        docs.extend(_parse_boot_log(boot_log))
-    else:
-        LOG.warn(
-            "Cannot parse boot log for %s - %s: boot report file does "
-            "not exists", job_dir, kernel_dir,
-        )
+        if not is_hidden(kernel) and os.path.isdir(kernel_dir):
+            for defconfig in os.listdir(kernel_dir):
+                defconfig_dir = os.path.join(kernel_dir, defconfig)
+
+                if not is_hidden(defconfig) and os.path.isdir(defconfig_dir):
+                    docs.extend([
+                        _parse_boot_log(boot_log, job, kernel, defconfig)
+                        for boot_log in glob.iglob(
+                            os.path.join(defconfig_dir, BOOT_REPORT_PATTERN)
+                        )
+                        if os.path.isfile(boot_log)
+                    ])
 
     return docs
 
 
-def _parse_boot_log(boot_log):
-    """Parse a boot log file.
+def _parse_boot_log(boot_log, job, kernel, defconfig):
+    """Read and parse the actual boot report.
 
-    The structure of the file makes so that this function will return a list
-    of documents, one for each defconfig found in the boot log file. Each
-    document will then contain a list with all the boards.
-
-    :param boot_log: Path to the boot log file. No checks are performed on it.
-    :return A list of boot documents.
+    :param boot_log: The path to the boot report.
+    :param job: The name of the job.
+    :param kernel: The name of the kernel.
+    :param defconfig: The name of the defconfig.
+    :return A `BootDocument` object.
     """
 
     LOG.info("Parsing boot log %s", boot_log)
+
+    boot_json = None
+    boot_doc = None
 
     created = datetime.fromtimestamp(
         os.stat(boot_log).st_mtime, tz=tz_util.utc
     )
 
-    # Necessary to define a BootDocument.
-    job = None
-    kernel = None
-    defconfig = None
+    board = os.path.basename(boot_log).\
+        replace('boot-', '').replace('.json', '')
 
-    # Sections inside the boot reporting file.
-    report_section = False
-    defconf_section = False
-    console_log_section = False
-    failed_log_section = False
+    with open(boot_log) as read_f:
+        boot_json = json.load(read_f)
 
-    # Where we store each boot_doc, the result of this function call.
-    boot_docs = []
-    boot_doc = None
+    if boot_json:
+        boot_doc = BootDocument(board, job, kernel, defconfig)
+        boot_doc.created_on = created
 
-    # Store the log lines in a list.
-    failed_log_lines = []
-    # Data structure to holde the failed logs identified via defconfig and
-    # board name.
-    failed_logs = {}
+        time_d = timedelta(seconds=float(boot_json.get(BOOT_TIME_JSON, 0.0)))
+        boot_time = datetime(
+            1970, 1, 1,
+            minute=time_d.seconds / 60,
+            second=time_d.seconds % 60,
+            microsecond=time_d.microseconds
+        )
 
-    # Need to scope these outside here to handle case when we reach EOF.
-    failed_board = None
-    failed_defconfig = None
-    log_lines = 0
+        boot_doc.time = boot_time
+        boot_doc.status = boot_json.get(BOOT_RESULT_JSON, UNKNOWN_STATUS)
+        boot_doc.warnings = boot_json.get(BOOT_WARNINGS_JSON, "0")
+        boot_doc.boot_log = boot_json.get(BOOT_LOG_KEY, None)
+        boot_doc.initrd_addr = boot_json.get(INITRD_ADDR_KEY, None)
+        boot_doc.load_addr = boot_json.get(LOAD_ADDR_JSON, None)
+        boot_doc.kernel_image = boot_json.get(KERNEL_IMAGE_KEY, None)
+        boot_doc.dtb_addr = boot_json.get(DTB_ADDR_KEY, None)
+        boot_doc.dtb = boot_json.get(DTB_KEY, None)
 
-    with open(boot_log) as read_boot:
-        for line in read_boot:
-            line = line.strip()
-
-            if line:
-                if RE_TREE_BRANCH.match(line):
-                    job = line.split(':')[1].strip()
-                elif RE_GIT_DESCRIBE.match(line):
-                    kernel = line.split(':')[1].strip()
-                elif RE_REPORT_SECTION.match(line):
-                    report_section = True
-                elif RE_CONSOLE_LOG_SECTION.match(line):
-                    LOG.info("Parsing failure log output section")
-                    console_log_section = True
-                    report_section = False
-                    defconf_section = False
-
-            if line.startswith('-') or line.startswith('='):
-                continue
-
-            if report_section:
-                if line:
-                    if not defconf_section:
-                        if RE_DECFONFIG_SECTION.match(line):
-                            defconfig = line.strip()
-                            defconf_section = True
-                    elif line and defconf_section:
-                        board, time, status, warnings = _parse_board_line(line)
-                        boot_doc = BootDocument(
-                            board, job, kernel, defconfig
-                        )
-                        boot_doc.created_on = created
-                        boot_doc.time = time
-                        boot_doc.status = status
-                        boot_doc.warnings = warnings
-
-                        boot_docs.append(boot_doc)
-                elif not line and defconf_section:
-                    # Done parsing the report section for this defconfig.
-                    defconf_section = False
-            elif console_log_section:
-                if line:
-                    if RE_DECFONFIG_SECTION.match(line):
-                        failed_defconfig = line.strip()
-                        failed_logs[failed_defconfig] = {}
-                    elif not failed_log_section:
-                        if RE_FAILED_LOG_SECTION.match(line):
-                            failed_board = line.split()[0].strip(':')
-
-                            failed_log_section = True
-                            failed_log_lines = []
-                            log_lines = 0
-                    elif failed_log_section:
-                        log_lines += 1
-                        failed_log_lines.append(line)
-                elif (not line and failed_log_section and log_lines > 0):
-                    if not failed_defconfig in failed_logs.keys():
-                        failed_logs[failed_defconfig] = {}
-
-                    if not failed_board in failed_logs[failed_defconfig].keys():
-                        failed_logs[failed_defconfig][failed_board] = None
-
-                    failed_logs[failed_defconfig][failed_board] = \
-                        failed_log_lines
-
-                    # Hard reset everything once done parsing the failed boot
-                    # log section.
-                    failed_log_section = False
-                    failed_board = None
-                    failed_log_lines = []
-                    log_lines = 0
-        else:
-            # Failed logs are at the end of the file, if we get there and there
-            # are no more lines at the end of the file, the last parsed failed
-            # log needs to be stored or it's lost.
-            if console_log_section and failed_log_section and log_lines > 0:
-                failed_logs[failed_defconfig][failed_board] = failed_log_lines
-
-            for doc in boot_docs:
-                for failed_defconf, failed_board in failed_logs.iteritems():
-                    if (doc.defconfig == failed_defconf and
-                            doc.board in failed_board.keys()):
-                        doc.fail_log = failed_board[doc.board]
-
-    return boot_docs
-
-
-def _parse_board_line(line):
-    """Very hackish way of parsing the board line.
-
-    This methods highly depends on how the boot log is built. If that changes
-    this can easily break.
-
-    :param line: The line to parse.
-    :return A tuple with board name, time taken to boot, the status, and the
-        number of warnings.
-    """
-    warnings = 0
-
-    values = line.split()
-    board = values.pop(0)
-
-    time_d = timedelta(
-        minutes=float(values[0]), seconds=float(values[2]))
-
-    # Boot duration is calculated as time after 1970-1-1 00:00:00.
-    time = datetime(
-        1970, 1, 1,
-        minute=time_d.seconds / 60,
-        second=time_d.seconds % 60,
-        microsecond=time_d.microseconds
-    )
-
-    values = values[4:]
-
-    status = values.pop(0)
-    if len(values) > 1:
-        warnings = values[1].strip(')')
-
-    return (board, time, status, warnings)
+    return boot_doc
 
 
 def _import_all(base_path=BASE_PATH):
     """Handy function to import all boot logs."""
     boot_docs = []
 
-    for job_dir in os.listdir(base_path):
-        job_dir = os.path.join(base_path, job_dir)
+    for job in os.listdir(base_path):
+        job_dir = os.path.join(base_path, job)
 
-        if os.path.isdir(job_dir):
-            LOG.info("Importing boot logs from %s", job_dir)
-            boot_docs.extend(
-                _parse_boot_log(boot_log) for boot_log in glob.iglob(
-                    os.path.join(job_dir, BOOT_REPORT_PATTERN)
-                )
-                if os.path.isfile(boot_log)
-            )
+        for kernel in os.listdir(job_dir):
+            boot_docs.extend(_parse_boot(job, kernel, base_path))
 
     return boot_docs
 
@@ -292,8 +196,6 @@ if __name__ == '__main__':
     database = connection[DB_NAME]
 
     all_docs = _import_all()
-
-    for doc_set in all_docs:
-        save(database, doc_set)
+    save(database, all_docs)
 
     connection.disconnect()
--- app/utils/tests/test_bootimport.py
+++ app/utils/tests/test_bootimport.py
@@ -13,11 +13,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
 import logging
 import mongomock
 import os
-import types
+import tempfile
 import unittest
+
+from mock import patch, MagicMock, Mock
 
 from datetime import (
     datetime,
@@ -26,7 +29,8 @@ from datetime import (
 
 from models.boot import BootDocument
 from utils.bootimport import (
-    _parse_boot_log
+    _parse_boot_log,
+    parse_boot_from_json,
 )
 
 
@@ -36,62 +40,67 @@ class TestParseBoot(unittest.TestCase):
         logging.disable(logging.CRITICAL)
         self.db = mongomock.Database(mongomock.Connection(), 'kernel-ci')
 
-        self.sample_dir = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), 'samples'))
-        self.simple_boot_log = os.path.join(
-            self.sample_dir, 'sample_boot_simple.log')
-        self.complex_boot_log = os.path.join(
-            self.sample_dir, 'sample_boot_complex.log')
-        self.fail_boot_log = os.path.join(
-            self.sample_dir, 'sample_boot_with_fail.log')
+        self.boot_report = dict(
+            boot_log='boot-board-name.log',
+            boot_result='PASS',
+            boot_time=28.07,
+            boot_warnings=0,
+            dtb='dtb/dir',
+            dtb_addr='0x81f00000',
+            initr_addr='0x81f00001',
+            kernel_image='zImage',
+            loadaddr='0x80200000',
+        )
 
     def tearDown(self):
         logging.disable(logging.NOTSET)
 
-    def test_import_parse_simple(self):
-        docs = _parse_boot_log(self.simple_boot_log)
-
-        self.assertEqual(len(docs), 2)
-        self.assertIsInstance(docs[0], BootDocument)
-
-        self.assertEqual(
-            docs[0].name,
-            'legacy,dm365evm-next-next-20140505-arm-davinci_all_defconfig'
+    def test_parse_boot_log(self):
+        temp_json_f = os.path.join(
+            tempfile.gettempdir(), 'boot-board-name.json'
         )
 
-        self.assertEqual(docs[0].board, 'legacy,dm365evm')
-        self.assertEqual(docs[0].job, 'next')
-        self.assertEqual(docs[0].kernel, 'next-20140505')
-        self.assertEqual(docs[0].defconfig, 'arm-davinci_all_defconfig')
+        try:
+            with open(temp_json_f, 'w') as w_f:
+                w_f.write(json.dumps(self.boot_report))
 
-        self.assertIsInstance(docs[0].created_on, datetime)
-        self.assertIsInstance(docs[0].time, datetime)
+            doc = _parse_boot_log(temp_json_f, 'job', 'kernel', 'defconfig')
 
-        time_d = timedelta(minutes=0, seconds=17.7)
-        time = datetime(
-            1970, 1, 1,
-            minute=time_d.seconds / 60,
-            second=time_d.seconds % 60,
-            microsecond=time_d.microseconds
-        )
+            time_d = timedelta(seconds=28.07)
+            boot_time = datetime(
+                1970, 1, 1,
+                minute=time_d.seconds / 60,
+                second=time_d.seconds % 60,
+                microsecond=time_d.microseconds
+            )
 
-        self.assertEqual(docs[0].time, time)
-        self.assertEqual(docs[0].warnings, '1')
+            self.assertIsInstance(doc, BootDocument)
+            self.assertEqual(doc.board, 'board-name')
+            self.assertEqual(doc.job, 'job')
+            self.assertEqual(doc.kernel, 'kernel')
+            self.assertEqual(doc.defconfig, 'defconfig')
+            self.assertIsInstance(doc.time, datetime)
+            self.assertEqual(doc.time, boot_time)
+            self.assertEqual(doc.boot_log, 'boot-board-name.log')
+            self.assertEqual(doc.status, 'PASS')
+            self.assertEqual(doc.load_addr, '0x80200000')
+        finally:
+            os.unlink(temp_json_f)
 
-    def test_import_parse_complex(self):
-        docs = _parse_boot_log(self.complex_boot_log)
+    @patch('utils.bootimport._parse_boot_log')
+    @patch('os.path.isfile')
+    @patch('glob.iglob', new=Mock(return_value=['boot-board.json']))
+    @patch('os.path.isdir')
+    @patch('os.listdir')
+    def test_parse_from_json_simple(
+            self, mock_listdir, mock_isdir, mock_isfile, mock_parse):
+        json_obj = dict(job='job', kernel='kernel')
 
-        self.assertEqual(len(docs), 49)
+        mock_isfile.return_value = True
+        mock_isdir.return_value = True
+        mock_parse.side_effect = [MagicMock(), MagicMock()]
+        mock_listdir.return_value = ('.hidden', 'defconfdir')
 
-    def test_import_parse_failed(self):
-        docs = _parse_boot_log(self.fail_boot_log)
+        docs = parse_boot_from_json(json_obj, base_path=tempfile.gettempdir())
 
-        self.assertEqual(len(docs), 20)
-
-        self.assertEqual(docs[18].status, 'FAIL')
-        self.assertIsInstance(docs[18].fail_log, types.ListType)
-        self.assertEqual(len(docs[18].fail_log), 80)
-
-        self.assertEqual(docs[16].status, 'FAIL')
-        self.assertIsInstance(docs[16].fail_log, types.ListType)
-        self.assertEqual(len(docs[16].fail_log), 80)
+        self.assertEqual(len(docs), 1)
