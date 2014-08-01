f4ca64ad818eb064af3e09209f7c5096c4596a74
Milo Casagrande <milo.casagrande@linaro.org>
2014-08-01 16:15:57 +0200

    Add config file and command line options.
      * Add at the server startup config file and command line
        options parsing. Command line options have higher priority.
      * Changes are necessary to provide custom master key and to activate
        or de-activate debug options.
    
    Change-Id: I491ee878e9223d8c3fdac1ec144ff83c800b8d70

--- app/server.py
+++ app/server.py
@@ -16,14 +16,32 @@
 
 """The Tornado application base module."""
 
+import os
 import pymongo
 import tornado
 
 from concurrent.futures import ThreadPoolExecutor
+from tornado.options import (
+    define,
+    options,
+)
 from tornado.web import Application
+from uuid import uuid4
 
 from handlers.app import AppHandler
 from urls import APP_URLS
+
+
+DEFAULT_CONFIG_FILE = '/etc/linaro/kernelci-backend.cfg'
+
+define('master_key', default=str(uuid4()), type=str, help="The master key")
+define(
+    'max_workers', default=20, type=int,
+    help="The number of workers for the thread pool executor"
+)
+define('gzip', default=True)
+define('debug', default=True)
+define('autoreload', default=True)
 
 
 class KernelCiBackend(Application):
@@ -39,14 +57,21 @@ class KernelCiBackend(Application):
         settings = {
             'client': self.mongodb_client,
             'default_handler_class': AppHandler,
-            'executor': ThreadPoolExecutor(max_workers=15),
-            'gzip': True,
-            'debug': True,
+            'executor': ThreadPoolExecutor(max_workers=options.max_workers),
+            'gzip': options.gzip,
+            'debug': options.debug,
+            'master_key': options.master_key,
+            'autoreload': options.autoreload,
         }
 
         super(KernelCiBackend, self).__init__(APP_URLS, **settings)
 
 
 if __name__ == '__main__':
+    if os.path.isfile(DEFAULT_CONFIG_FILE):
+        options.parse_config_file(DEFAULT_CONFIG_FILE, final=False)
+
+    options.parse_command_line()
+
     KernelCiBackend().listen(8888)
     tornado.ioloop.IOLoop.instance().start()
