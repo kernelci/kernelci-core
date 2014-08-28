fbe71bc38bf5742d7301df4398105ba138eea135
Tyler <tyler.baker@linaro.org>
2014-08-28 10:23:05 -0700

    kernel-ci: create device datastructures for dtb mapping

--- lava-kernel-ci-job-creator.py
+++ lava-kernel-ci-job-creator.py
@@ -9,12 +9,57 @@ import shutil
 base_url = None
 kernel = None
 dtb_list = []
-dtb_map = {'exynos5250-arndale.dtb': 'arndale',
-           'am335x-boneblack.dtb': 'beaglebone-black',
-           'omap3-beagle-xm.dtb': 'beagle-xm',
-           'omap4-panda-es.dtb': 'panda',
-           'imx6dl-wandboard.dtb': 'imx6q-wandboard',
-           'imx6q-wandboard.dtb': 'imx6q-wandboard'}
+
+arndale = {'device_type': 'arndale',
+           'template': 'kernel-ci-template.json',
+           'defconfig_blacklist': [],
+           'lpae': True,
+           'be': False}
+
+arndale_octa = {'device_type': 'arndale-octa',
+                'template': 'kernel-ci-template.json',
+                'defconfig_blacklist': [],
+                'lpae': True,
+                'be': False}
+
+beaglebone_black = {'device_type': 'beaglebone-black',
+                    'template': 'kernel-ci-template.json',
+                    'defconfig_blacklist': [],
+                    'lpae': False,
+                    'be': False}
+
+beagle_xm = {'device_type': 'beagle-xm',
+             'template': 'kernel-ci-template.json',
+            'defconfig_blacklist': [],
+             'lpae': False,
+             'be': False}
+
+panda_es = {'device_type': 'panda-es',
+            'template': 'kernel-ci-template.json',
+            'defconfig_blacklist': [],
+            'lpae': False,
+            'be': False}
+
+cubieboard3 = {'device_type': 'cubieboard3',
+               'template': 'kernel-ci-template.json',
+               'defconfig_blacklist': [],
+               'lpae': True,
+               'be': False}
+
+imx6q_wandboard = {'device_type': 'imx6q-wandboard',
+                   'template': 'kernel-ci-template.json',
+                   'defconfig_blacklist': ['arm-imx_v4_v5_defconfig',
+                                           'arm-multi_v5_defconfig'],
+                   'lpae': False,
+                   'be': False}
+
+dtb_map = {'exynos5250-arndale.dtb': arndale,
+           'exynos5420-arndale-octa.dtb': arndale_octa,
+           'am335x-boneblack.dtb': beaglebone_black,
+           'omap3-beagle-xm.dtb': beagle_xm,
+           'omap4-panda-es.dtb': panda_es,
+           'sun7i-a20-cubietruck.dtb': cubieboard3,
+           'imx6q-wandboard.dtb': imx6q_wandboard}
 
 parse_re = re.compile('href="([^"]*)".*(..-...-.... ..:..).*?(\d+[^\s<]*|-)')
 
@@ -41,26 +86,37 @@ def create_jobs(base_url, kernel, dtb_list):
 
     for dtb in dtb_list:
         dtb_name = dtb.split('/')[-1]
-        device_type = dtb_map[dtb_name]
-        job_name = tree + '-' + kernel_version + '-' + defconfig + '-' + dtb_name
-        job_json = cwd + '/jobs/' + job_name + '.json'
-        template_file = cwd + '/templates/kernel-ci-template.json'
-        with open(job_json, 'wt') as fout:
-            with open(template_file, "rt") as fin:
-                for line in fin:
-                    tmp = line.replace('{dtb_url}', dtb)
-                    tmp = tmp.replace('{kernel_url}', kernel)
-                    #tmp = tmp.replace('{lava_server}', lava_server)
-                    #tmp = tmp.replace('{bundle_stream}', bundle_stream)
-                    tmp = tmp.replace('{device_type}', device_type)
-                    tmp = tmp.replace('{job_name}', job_name)
-                    tmp = tmp.replace('{image_type}', image_type)
-                    tmp = tmp.replace('{image_url}', image_url)
-                    tmp = tmp.replace('{tree}', tree)
-                    tmp = tmp.replace('{kernel_version}', kernel_version)
-                    tmp = tmp.replace('{defconfig}', defconfig)
-                    fout.write(tmp)
-        print 'JSON Job created: jobs/%s' % job_name
+        device = dtb_map[dtb_name]
+        device_type = device['device_type']
+        device_template = device['template']
+        lpae = device['lpae']
+        be = device['be']
+        if 'BIG_ENDIAN' in defconfig and not be:
+            print 'BIG_ENDIAN is not supported on %s. Skipping JSON creation' % device_type
+        elif 'LPAE' in defconfig and not lpae:
+            print 'LPAE is not supported on %s. Skipping JSON creation' % device_type
+        elif defconfig in device['defconfig_blacklist']:
+            print '%s has been blacklisted. Skipping JSON creation' % defconfig
+        else:
+            job_name = tree + '-' + kernel_version + '-' + defconfig + '-' + dtb_name
+            job_json = cwd + '/jobs/' + job_name + '.json'
+            template_file = cwd + '/templates/' + str(device_template)
+            with open(job_json, 'wt') as fout:
+                with open(template_file, "rt") as fin:
+                    for line in fin:
+                        tmp = line.replace('{dtb_url}', dtb)
+                        tmp = tmp.replace('{kernel_url}', kernel)
+                        #tmp = tmp.replace('{lava_server}', lava_server)
+                        #tmp = tmp.replace('{bundle_stream}', bundle_stream)
+                        tmp = tmp.replace('{device_type}', device_type)
+                        tmp = tmp.replace('{job_name}', job_name)
+                        tmp = tmp.replace('{image_type}', image_type)
+                        tmp = tmp.replace('{image_url}', image_url)
+                        tmp = tmp.replace('{tree}', tree)
+                        tmp = tmp.replace('{kernel_version}', kernel_version)
+                        tmp = tmp.replace('{defconfig}', defconfig)
+                        fout.write(tmp)
+            print 'JSON Job created: jobs/%s' % job_name
 
 
 def walk_url(url):
