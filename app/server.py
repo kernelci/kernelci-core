27c5e61b102e9a520a640d7d44b35866c694de47
Milo Casagrande <milo.casagrande@linaro.org>
2014-09-09 13:52:39 +0200

    server: Removed not-welcome parameters.
      * HTTPServer does not accept all parameters.
    
    Change-Id: I08cf87c6e988d92daca9c8674238f6e2aca4d5d9

--- app/server.py
+++ app/server.py
@@ -79,9 +79,6 @@ if __name__ == '__main__':
     # Settings that should be passed also to the HTTPServer.
     http_settings = {
         'xheaders': True,
-        'gzip': options.gzip,
-        'debug': options.debug,
-        'autoreload': options.autoreload,
     }
 
     KernelCiBackend().listen(8888, **http_settings)
