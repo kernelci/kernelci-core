fd7c3876235839243d63aabf2010ef95fc50680f
Tyler Baker <tyler.baker@linaro.org>
2014-10-30 14:35:26 -0700

    kernel-ci: add json format boot metadata

--- lava-job-runner.py
+++ lava-job-runner.py
@@ -34,28 +34,78 @@ def kreport(connection, jobs, duration):
         binary_job_file = connection.scheduler.job_output(job_id)
         job_file = str(binary_job_file)
         # Retrieve bundle
-        json_bundle = connection.dashboard.get(bundle)
-        bundle_data = json.loads(json_bundle['content'])
-        if bundle_data['test_runs'][0]['attributes']['kernel.defconfig']:
-            defconfig = bundle_data['test_runs'][0]['attributes']['kernel.defconfig']
-        if bundle_data['test_runs'][0]['attributes']['kernel.version']:
-            kernel_version = bundle_data['test_runs'][0]['attributes']['kernel.version']
-        if bundle_data['test_runs'][0]['attributes']['kernel-boot-time']:
-            kernel_boot_time = bundle_data['test_runs'][0]['attributes']['kernel-boot-time']
-        if bundle_data['test_runs'][0]['attributes']['kernel.tree']:
-            kernel_tree = bundle_data['test_runs'][0]['attributes']['kernel.tree']
-        # Record the results
+        if bundle is not None:
+            json_bundle = connection.dashboard.get(bundle)
+            bundle_data = json.loads(json_bundle['content'])
+            bundle_attributes =  bundle_data['test_runs'][0]['attributes']
+            boot_meta = {}
+            kernel_defconfig = None
+            kernel_version = None
+            kernel_boot_time = None
+            kernel_tree = None
+            kernel_image = None
+            kernel_addr = None
+            initrd_addr = None
+            dtb_addr = None
+            dtb_append = None
+            if in_bundle_attributes(bundle_attributes, 'kernel.defconfig'):
+                kernel_defconfig = bundle_attributes['kernel.defconfig']
+            if in_bundle_attributes(bundle_attributes, 'kernel.version'):
+                kernel_version = bundle_attributes['kernel.version']
+            if in_bundle_attributes(bundle_attributes, 'kernel-boot-time'):
+                kernel_boot_time = bundle_attributes['kernel-boot-time']
+            if in_bundle_attributes(bundle_attributes, 'kernel.tree'):
+                kernel_tree = bundle_attributes['kernel.tree']
+            if in_bundle_attributes(bundle_attributes, 'kernel-image'):
+                kernel_image = bundle_attributes['kernel-image']
+            if in_bundle_attributes(bundle_attributes, 'kernel-addr'):
+                kernel_addr = bundle_attributes['kernel-addr']
+            if in_bundle_attributes(bundle_attributes, 'initrd-addr'):
+                initrd_addr = bundle_attributes['initrd-addr']
+            if in_bundle_attributes(bundle_attributes, 'dtb-addr'):
+                dtb_addr = bundle_attributes['dtb-addr']
+            if in_bundle_attributes(bundle_attributes, 'dtb-append'):
+                dtb_append = bundle_attributes['dtb-append']
+        # Record the boot log and result
         # TODO: Will need to map device_types to dashboard device types
-        if defconfig and device_type and kernel_boot_time and result:
+        if kernel_defconfig and device_type and kernel_boot_time and result:
+            print 'Creating boot log for %s' % device_type
             log = 'boot-%s.log' % device_type
-            directory = os.path.join(results_directory, defconfig)
+            directory = os.path.join(results_directory, kernel_defconfig)
             mkdir(directory)
             write_file(job_file, log, directory)
-            if results.has_key(defconfig):
-                results[defconfig].append({'device_type': device_type, 'kernel_boot_time': kernel_boot_time, 'result': result})
+            if results.has_key(kernel_defconfig):
+                results[kernel_defconfig].append({'device_type': device_type, 'kernel_boot_time': kernel_boot_time, 'result': result})
             else:
-                results[defconfig] = [{'device_type': device_type, 'kernel_boot_time': kernel_boot_time, 'result': result}]
+                results[kernel_defconfig] = [{'device_type': device_type, 'kernel_boot_time': kernel_boot_time, 'result': result}]
+            # Create JSON format boot metadata
+            print 'Creating JSON format boot metadata'
+            boot_meta['boot_log'] = log
+            # TODO: Fix this
+            boot_meta['boot_log_html'] = None
+            boot_meta['boot_result'] = result
+            boot_meta['boot_time'] = kernel_boot_time
+            # TODO: Fix this
+            boot_meta['boot_warnings'] = None
+            # TODO: Fix this
+            boot_meta['dtb'] = None
+            boot_meta['dtb_addr'] = dtb_addr
+            boot_meta['dtb_append'] = dtb_append
+            # TODO: Fix this
+            boot_meta['endian'] = None
+            # TODO: Fix this
+            boot_meta['initrd'] = None
+            boot_meta['initrd_addr'] = initrd_addr
+            boot_meta['kernel_image'] = kernel_image
+            boot_meta['loadaddr'] = kernel_addr
+            json_file = 'boot-%s.json' % device_type
+            json_path = os.path.join(directory, json_file)
+            boot_json_f = open(json_path, 'w')
+            json.dump(boot_meta, boot_json_f, indent=4, sort_keys=True)
+            boot_json_f.close()
+
     if results and kernel_tree and kernel_version:
+        print 'Creating boot summary for %s' % kernel_version
         log = '%s-boot-report.log' % kernel_version
         with open(os.path.join(results_directory, log), 'a') as f:
             f.write('Status Dashboard: http://status.armcloud.us/boot/all/job/%s/kernel/%s/\n' % (kernel_tree, kernel_version))
@@ -82,6 +132,12 @@ def mkdir(directory):
     else:
         shutil.rmtree(directory)
         os.makedirs(directory)
+
+def in_bundle_attributes(bundle_atrributes, key):
+    if key in bundle_atrributes:
+        return True
+    else:
+        return False
 
 def poll_jobs(connection):
     run = True
