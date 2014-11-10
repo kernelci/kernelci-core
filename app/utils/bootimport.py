a9f8a5c64b22fbb1680cc5e521809c29868f8b17
Milo Casagrande <milo.casagrande@linaro.org>
2014-11-10 17:51:19 +0100

    bootimport: Refactor import logic and fix tests.
    Change-Id: I909d2345c6095b31ae493b7484f0145182316e46

--- app/utils/bootimport.py
+++ app/utils/bootimport.py
@@ -16,11 +16,11 @@
 """Container for all the boot import related functions."""
 
 import bson
+import copy
 import datetime
 import glob
 import json
 import os
-import pymongo
 import re
 
 import models
@@ -36,7 +36,7 @@ BOOT_REPORT_PATTERN = 'boot-*.json'
 TMP_RE = re.compile(r'tmp')
 
 
-def import_and_save_boot(json_obj, db_options):
+def import_and_save_boot(json_obj, db_options, base_path=utils.BASE_PATH):
     """Wrapper function to be used as an external task.
 
     This function should only be called by Celery or other task managers.
@@ -49,39 +49,197 @@ def import_and_save_boot(json_obj, db_options):
     :param db_options: The mongodb database connection parameters.
     :type db_options: dict
     """
-    docs = parse_boot_from_json(json_obj, utils.BASE_PATH)
+    json_copy = copy.deepcopy(json_obj)
+    doc = _parse_boot_from_json(json_copy)
+    doc_id = None
+    ret_code = None
 
-    if docs:
+    if doc:
         database = db.get_db_connection(db_options)
-        db.save(database, docs)
+        ret_code, doc_id = db.save(database, doc, manipulate=True)
+        save_to_disk(doc, json_obj, base_path)
     else:
-        utils.LOG.info("No boot log imported")
+        utils.LOG.info("Boot report not imported nor saved")
+
+    return ret_code, doc_id
 
 
-def parse_boot_from_json(json_obj, base_path=utils.BASE_PATH):
-    """Parse boot log file from a JSON object.
+def save_to_disk(boot_doc, json_obj, base_path):
+    """Save the provided boot report to disk.
 
-    The provided JSON object, a dict-like object, should contain at least the
-    `job` and `kernel` keys.
-
-    :param json_obj: A dict-like object that should contain the keys `job` and
-    :param base_path: The base path where to start looking for the boot log
-        file. It defaults to: /var/www/images/kernel-ci.
-    :return A list with all the `BootDocument`s.
+    :param boot_doc: The document parsed.
+    :type boot_doc: models.boot.BootDocument
+    :param json_obj: The JSON object to save.
+    :type json_obj: dict
+    :param base_path: The base path where to save the document.
+    :type base_path: str
     """
-    json_get = json_obj.get
-    job = json_get(models.JOB_KEY)
-    kernel = json_get(models.KERNEL_KEY)
-    defconfig = json_get(models.DEFCONFIG_KEY)
-    lab_id = json_get(models.LAB_ID_KEY)
+    job = boot_doc.job
+    kernel = boot_doc.kernel
+    defconfig = boot_doc.defconfig
+    lab_id = boot_doc.lab_id
+    board = boot_doc.board
 
-    return _parse_boot_from_disk(
-        job, kernel, defconfig, lab_id, base_path
+    dir_path = os.path.join(base_path, job, kernel, defconfig, lab_id)
+    file_path = os.path.join(dir_path, 'boot-%s.json' % board)
+
+    try:
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+        with open(file_path, mode="w") as write_json:
+            write_json.write(json.dumps(json_obj, encoding="utf_8"))
+    except OSError, ex:
+        utils.LOG.error(
+            "Error saving document '%s' into '%s'", boot_doc.name, dir_path
+        )
+        utils.LOG.exception(ex)
+
+
+def _parse_boot_from_file(boot_log, job, kernel, defconfig, lab_id):
+    """Read and parse the actual boot report.
+
+    :param boot_log: The path to the boot report.
+    :param job: The name of the job.
+    :param kernel: The name of the kernel.
+    :param defconfig: The name of the defconfig.
+    :return A `BootDocument` object.
+    """
+
+    utils.LOG.info("Parsing boot log '%s'", os.path.basename(boot_log))
+
+    boot_json = None
+    boot_doc = None
+
+    with open(boot_log) as read_f:
+        boot_json = json.load(read_f)
+
+    json_pop_f = boot_json.pop
+
+    job = json_pop_f(models.JOB_KEY)
+    kernel = json_pop_f(models.KERNEL_KEY)
+    defconfig = json_pop_f(models.DEFCONFIG_KEY)
+    lab_id = json_pop_f(models.LAB_ID_KEY)
+    dtb = boot_json.get(models.DTB_KEY, None)
+
+    board = json_pop_f(models.BOARD_KEY, None)
+    if not board:
+        utils.LOG.info("No board value specified in the boot report")
+        if dtb and not TMP_RE.findall(dtb):
+            board = os.path.splitext(os.path.basename(dtb))[0]
+        else:
+            # If we do not have the dtb field we use the boot report file to
+            # extract some kind of value for board.
+            board = os.path.splitext(
+                os.path.basename(boot_log).replace('boot-', ''))[0]
+            utils.LOG.info(
+                "Using boot report file name for board name: %s", board
+            )
+
+    boot_doc = modbt.BootDocument(board, job, kernel, defconfig, lab_id)
+    _update_boot_doc_from_json(boot_doc, boot_json, json_pop_f)
+
+    return boot_doc
+
+
+def _parse_boot_from_json(boot_json):
+    """Parse the boot report from a JSON object.
+
+    :param boot_json: The JSON object.
+    :type boot_json: dict
+    :return A `models.boot.BootDocument` instance, or None if the JSON cannot
+    be parsed correctly.
+    """
+    boot_doc = None
+
+    try:
+        json_pop_f = boot_json.pop
+        board = json_pop_f(models.BOARD_KEY)
+        job = json_pop_f(models.JOB_KEY)
+        kernel = json_pop_f(models.KERNEL_KEY)
+        defconfig = json_pop_f(models.DEFCONFIG_KEY)
+        lab_id = json_pop_f(models.LAB_ID_KEY)
+
+        boot_doc = modbt.BootDocument(board, job, kernel, defconfig, lab_id)
+        boot_doc.created_on = datetime.datetime.now(tz=bson.tz_util.utc)
+        _update_boot_doc_from_json(boot_doc, boot_json, json_pop_f)
+    except KeyError, ex:
+        utils.LOG.error(
+            "Missing key in boot report: import failed"
+        )
+        utils.LOG.exception(ex)
+
+    return boot_doc
+
+
+def _update_boot_doc_from_json(boot_doc, boot_json, json_pop_f):
+    """Update a BootDocument from the provided JSON boot object.
+
+    This function does not return anything, and the BootDocument passed is
+    updated from the values found in the provided JSON object.
+
+    :param boot_doc: The BootDocument to update.
+    :type boot_doc: `models.boot.BootDocument`.
+    :param boot_json: The JSON object from where to take that parameters.
+    :type boot_json: dict
+    :param json_pop_f: The function used to pop elements out of the JSON object.
+    :type json_pop_f: function
+    """
+    time_d = datetime.timedelta(
+        seconds=float(json_pop_f(models.BOOT_TIME_KEY, 0.0))
+    )
+    boot_doc.time = datetime.datetime(
+        1970, 1, 1,
+        minute=time_d.seconds / 60,
+        second=time_d.seconds % 60,
+        microsecond=time_d.microseconds
     )
 
+    boot_doc.status = json_pop_f(
+        models.BOOT_RESULT_KEY, models.UNKNOWN_STATUS
+    )
+    boot_doc.warnings = json_pop_f(models.BOOT_WARNINGS_KEY, 0)
+    boot_doc.boot_log = json_pop_f(models.BOOT_LOG_KEY, None)
+    boot_doc.initrd_addr = json_pop_f(models.INITRD_ADDR_KEY, None)
+    boot_doc.load_addr = json_pop_f(models.BOOT_LOAD_ADDR_KEY, None)
+    boot_doc.kernel_image = json_pop_f(models.KERNEL_IMAGE_KEY, None)
+    boot_doc.dtb_addr = json_pop_f(models.DTB_ADDR_KEY, None)
+    boot_doc.endianness = json_pop_f(models.ENDIANNESS_KEY, None)
+    boot_doc.boot_log_html = json_pop_f(models.BOOT_LOG_HTML_KEY, None)
+    boot_doc.fastboot = json_pop_f(models.FASTBOOT_KEY, None)
+    boot_doc.boot_result_description = json_pop_f(
+        models.BOOT_RESULT_DESC_KEY, None
+    )
+    boot_doc.retries = json_pop_f(models.BOOT_RETRIES_KEY, 0)
+    boot_doc.dtb = json_pop_f(models.DTB_KEY, None)
+    boot_doc.version = json_pop_f(models.VERSION_KEY, "1.0")
 
-def _parse_boot_from_disk(
-        job, kernel, defconfig, lab_id, base_path=utils.BASE_PATH):
+    boot_doc.metadata = boot_json
+
+
+def import_all_for_lab(lab_id, base_path=utils.BASE_PATH):
+    """Handy function to import all boot logs.
+
+    :param lab_id: The lab ID whose boot reports should be imported.
+    :type lab_id: str
+    :param base_path: Where to start the scan on the hard disk.
+    :type base_path: str
+    :return A list of BootDocument documents.
+    """
+    boot_docs = []
+
+    for job in os.listdir(base_path):
+        job_dir = os.path.join(base_path, job)
+
+        for kernel in os.listdir(job_dir):
+            boot_docs.extend(
+                parse_boot_from_disk(job, kernel, lab_id, base_path)
+            )
+
+    return boot_docs
+
+
+def parse_boot_from_disk(job, kernel, lab_id, base_path=utils.BASE_PATH):
     """Traverse the kernel directory and look for boot report logs.
 
     :param job: The name of the job.
@@ -102,113 +260,17 @@ def _parse_boot_from_disk(
 
                 if not utils.is_hidden(defconfig) and \
                         os.path.isdir(defconfig_dir):
-                    docs.extend([
-                        _parse_boot_log(boot_log, job, kernel, defconfig)
-                        for boot_log in glob.iglob(
-                            os.path.join(defconfig_dir, BOOT_REPORT_PATTERN)
-                        )
-                        if os.path.isfile(boot_log)
-                    ])
+
+                    lab_dir = os.path.join(defconfig_dir, lab_id)
+                    if os.path.isdir(lab_dir):
+                        docs.extend([
+                            _parse_boot_from_file(
+                                boot_log, job, kernel, defconfig
+                            )
+                            for boot_log in glob.iglob(
+                                os.path.join(lab_dir, BOOT_REPORT_PATTERN)
+                            )
+                            if os.path.isfile(boot_log)
+                        ])
 
     return docs
-
-
-def _parse_boot_log(boot_log, job, kernel, defconfig):
-    """Read and parse the actual boot report.
-
-    :param boot_log: The path to the boot report.
-    :param job: The name of the job.
-    :param kernel: The name of the kernel.
-    :param defconfig: The name of the defconfig.
-    :return A `BootDocument` object.
-    """
-
-    utils.LOG.info("Parsing boot log '%s'", os.path.basename(boot_log))
-
-    boot_json = None
-    boot_doc = None
-
-    with open(boot_log) as read_f:
-        boot_json = json.load(read_f)
-
-    if boot_json:
-        json_pop = boot_json.pop
-        lab_id = json_pop(models.LAB_ID_KEY, "")
-        dtb = json_pop(models.DTB_KEY, None)
-
-        if dtb and not TMP_RE.findall(dtb):
-            board = os.path.splitext(os.path.basename(dtb))[0]
-        else:
-            # If we do not have the dtb field we use the boot report file to
-            # extract some kind of value for board.
-            board = os.path.splitext(
-                os.path.basename(boot_log).replace('boot-', ''))[0]
-            utils.LOG.info(
-                "Using boot report file name for board name: %s", board
-            )
-
-        # FIXME: need to pass the lab_id.
-        boot_doc = modbt.BootDocument(board, job, kernel, defconfig, lab_id)
-        boot_doc.created_on = datetime.datetime.fromtimestamp(
-            os.stat(boot_log).st_mtime, tz=bson.tz_util.utc)
-
-        time_d = datetime.timedelta(
-            seconds=float(json_pop(models.BOOT_TIME_KEY, 0.0))
-        )
-        boot_doc.time = datetime.datetime(
-            1970, 1, 1,
-            minute=time_d.seconds / 60,
-            second=time_d.seconds % 60,
-            microsecond=time_d.microseconds
-        )
-
-        boot_doc.status = json_pop(
-            models.BOOT_RESULT_KEY, models.UNKNOWN_STATUS
-        )
-        boot_doc.warnings = json_pop(models.BOOT_WARNINGS_KEY, "0")
-        boot_doc.boot_log = json_pop(models.BOOT_LOG_KEY, None)
-        boot_doc.initrd_addr = json_pop(models.INITRD_ADDR_KEY, None)
-        boot_doc.load_addr = json_pop(models.BOOT_LOAD_ADDR_KEY, None)
-        boot_doc.kernel_image = json_pop(models.KERNEL_IMAGE_KEY, None)
-        boot_doc.dtb_addr = json_pop(models.DTB_ADDR_KEY, None)
-        boot_doc.endianness = json_pop(models.ENDIANNESS_KEY, None)
-        boot_doc.boot_log_html = json_pop(models.BOOT_LOG_HTML_KEY, None)
-        boot_doc.fastboot = json_pop(models.FASTBOOT_KEY, None)
-        boot_doc.boot_result_description = json_pop(
-            models.BOOT_RESULT_DESC_KEY, None
-        )
-        boot_doc.retries = json_pop(models.BOOT_RETRIES_KEY, None)
-        boot_doc.dtb = dtb
-
-        boot_doc.metadata = boot_json
-    else:
-        utils.LOG.error(
-            "Boot log '%s' does not contain JSON data",
-            os.path.basename(boot_log)
-        )
-
-    return boot_doc
-
-
-def _import_all(base_path=utils.BASE_PATH):
-    """Handy function to import all boot logs."""
-    boot_docs = []
-
-    for job in os.listdir(base_path):
-        job_dir = os.path.join(base_path, job)
-
-        for kernel in os.listdir(job_dir):
-            boot_docs.extend(_parse_boot(job, kernel, base_path))
-
-    return boot_docs
-
-
-# pylint: disable=invalid-name
-if __name__ == '__main__':
-    connection = pymongo.MongoClient()
-    loc_db = connection[models.DB_NAME]
-
-    all_docs = _import_all()
-    db.save(loc_db, all_docs)
-
-    connection.disconnect()
--- app/utils/tests/test_bootimport.py
+++ app/utils/tests/test_bootimport.py
@@ -13,25 +13,17 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
 import logging
 import mongomock
 import os
 import tempfile
+import types
 import unittest
 
-from mock import patch, MagicMock, Mock
+from mock import patch
 
-from datetime import (
-    datetime,
-    timedelta,
-)
-
-from models.boot import BootDocument
-from utils.bootimport import (
-    _parse_boot_log,
-    parse_boot_from_json,
-)
+import models.boot as modb
+import utils.bootimport
 
 
 class TestParseBoot(unittest.TestCase):
@@ -39,112 +31,77 @@ class TestParseBoot(unittest.TestCase):
     def setUp(self):
         logging.disable(logging.CRITICAL)
         self.db = mongomock.Database(mongomock.Connection(), 'kernel-ci')
+        self.base_path = tempfile.gettempdir()
 
         self.boot_report = dict(
+            version="1.0",
+            board="board",
+            lab_id="lab_id",
+            kernel="kernel",
+            job="job",
+            defconfig="defconfig",
             boot_log='boot-board-name.log',
-            boot_result='PASS',
+            boot_result="PASS",
+            boot_result_description="passed",
             boot_time=28.07,
             boot_warnings=0,
-            dtb='dtb/board-name.dtb',
-            dtb_addr='0x81f00000',
-            initrd_addr='0x81f00001',
-            kernel_image='zImage',
-            loadaddr='0x80200000',
-            endian='little',
+            dtb="dtb/board-name.dtb",
+            dtb_addr="0x81f00000",
+            initrd_addr="0x81f00001",
+            kernel_image="zImage",
+            loadaddr="0x80200000",
+            endian="little",
             uImage=True,
-            uimage_addr='xip'
+            uimage_addr="xip"
         )
 
     def tearDown(self):
         logging.disable(logging.NOTSET)
 
-    def test_parse_boot_log(self):
-        temp_json_f = os.path.join(
-            tempfile.gettempdir(), 'boot-board-name.json'
+    def test_parse_from_json_simple(self):
+        doc = utils.bootimport._parse_boot_from_json(self.boot_report)
+
+        self.assertIsInstance(doc, modb.BootDocument)
+        self.assertEqual(doc.name, "board-job-kernel-defconfig")
+        self.assertEqual(doc.load_addr, "0x80200000")
+        self.assertEqual(doc.endianness, "little")
+        self.assertEqual(doc.version, "1.0")
+        self.assertIsInstance(doc.metadata, types.DictionaryType)
+
+    @patch("utils.db.get_db_connection")
+    def test_import_and_save_boot(self, mock_db):
+        mock_db = self.db
+
+        code, doc_id = utils.bootimport.import_and_save_boot(
+            self.boot_report, {}, base_path=self.base_path
         )
+        lab_dir = os.path.join(
+            self.base_path, "job", "kernel", "defconfig", "lab_id"
+        )
+        boot_file = os.path.join(lab_dir, "boot-board.json")
+
+        self.assertTrue(os.path.isdir(lab_dir))
+        self.assertTrue(os.path.isfile(boot_file))
+        self.assertEqual(code, 201)
 
         try:
-            with open(temp_json_f, 'w') as w_f:
-                w_f.write(json.dumps(self.boot_report))
+            os.remove(boot_file)
+            os.rmdir(lab_dir)
+        except OSError:
+            pass
 
-            doc = _parse_boot_log(temp_json_f, 'job', 'kernel', 'defconfig')
-
-            time_d = timedelta(seconds=28.07)
-            boot_time = datetime(
-                1970, 1, 1,
-                minute=time_d.seconds / 60,
-                second=time_d.seconds % 60,
-                microsecond=time_d.microseconds
-            )
-
-            self.assertIsInstance(doc, BootDocument)
-            self.assertEqual(doc.board, 'board-name')
-            self.assertEqual(doc.job, 'job')
-            self.assertEqual(doc.kernel, 'kernel')
-            self.assertEqual(doc.defconfig, 'defconfig')
-            self.assertIsInstance(doc.time, datetime)
-            self.assertEqual(doc.time, boot_time)
-            self.assertEqual(doc.boot_log, 'boot-board-name.log')
-            self.assertEqual(doc.status, 'PASS')
-            self.assertEqual(doc.load_addr, '0x80200000')
-            self.assertEqual(doc.initrd_addr, '0x81f00001')
-            self.assertEqual(doc.endianness, 'little')
-            self.assertDictEqual(
-                doc.metadata, {'uImage': True, 'uimage_addr': 'xip'}
-            )
-        finally:
-            os.unlink(temp_json_f)
-
-    def test_parse_boot_log_without_dtb(self):
-        temp_json_f = os.path.join(
-            tempfile.gettempdir(), 'boot-board-name.json'
+    def test_parse_from_json_wrong_json(self):
+        boot_json = {
+            "foo": "bar"
+        }
+        self.assertRaises(
+            KeyError, utils.bootimport._parse_boot_from_json(boot_json)
         )
 
-        try:
-            self.boot_report.pop('dtb')
+    @patch("utils.bootimport._parse_boot_from_json")
+    def test_import_and_save_no_doc(self, mock_parse):
+        mock_parse.return_value = None
 
-            with open(temp_json_f, 'w') as w_f:
-                w_f.write(json.dumps(self.boot_report))
-
-            doc = _parse_boot_log(temp_json_f, 'job', 'kernel', 'defconfig')
-
-            self.assertIsInstance(doc, BootDocument)
-            self.assertEqual(doc.board, 'board-name')
-        finally:
-            os.unlink(temp_json_f)
-
-    def test_parse_boot_log_with_tmp_dir(self):
-        temp_json_f = os.path.join(
-            tempfile.gettempdir(), 'boot-board-name.json'
-        )
-
-        try:
-            self.boot_report['dtb'] = '/tmp/tmpfoo-bar.dtb'
-
-            with open(temp_json_f, 'w') as w_f:
-                w_f.write(json.dumps(self.boot_report))
-
-            doc = _parse_boot_log(temp_json_f, 'job', 'kernel', 'defconfig')
-
-            self.assertIsInstance(doc, BootDocument)
-            self.assertEqual(doc.board, 'board-name')
-        finally:
-            os.unlink(temp_json_f)
-
-    @patch('utils.bootimport._parse_boot_log')
-    @patch('os.path.isfile')
-    @patch('glob.iglob', new=Mock(return_value=['boot-board.json']))
-    @patch('os.path.isdir')
-    @patch('os.listdir')
-    def test_parse_from_json_simple(
-            self, mock_listdir, mock_isdir, mock_isfile, mock_parse):
-        json_obj = dict(job='job', kernel='kernel')
-
-        mock_isfile.return_value = True
-        mock_isdir.return_value = True
-        mock_parse.side_effect = [MagicMock(), MagicMock()]
-        mock_listdir.return_value = ('.hidden', 'defconfdir')
-
-        docs = parse_boot_from_json(json_obj, base_path=tempfile.gettempdir())
-
-        self.assertEqual(len(docs), 1)
+        code, doc_id = utils.bootimport.import_and_save_boot({}, {})
+        self.assertIsNone(code)
+        self.assertIsNone(doc_id)
