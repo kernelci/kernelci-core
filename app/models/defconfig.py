83f4a809053b3429894c26f243ecdb1359bb41e1
Milo Casagrande <milo.casagrande@linaro.org>
2014-11-24 17:03:19 +0100

    defconfig: Clean up model code.
    Change-Id: If979cfbc9363a23432dcb31dbb6710d0a3f06e87

--- app/models/defconfig.py
+++ app/models/defconfig.py
@@ -35,40 +35,37 @@ class DefconfigDocument(modb.BaseDocument):
             models.DEFCONFIG_KEY: defconfig_full or defconfig
         }
 
-        self._name = models.DEFCONFIG_DOCUMENT_NAME % doc_name
-        self._id = None
         self._created_on = None
+        self._id = None
+        self._name = models.DEFCONFIG_DOCUMENT_NAME % doc_name
+        self._version = None
 
-        self._job = job
-        self._kernel = kernel
+        self._build_platform = []
         self._defconfig = defconfig
         self._defconfig_full = defconfig_full or defconfig
-        self._job_id = None
-        self._dirname = None
-        self._status = None
+        self._job = job
+        self._kernel = kernel
         self._metadata = {}
-        self._errors = 0
-        self._warnings = 0
-        self._build_time = 0
-        self._arch = None
-
-        self._git_url = None
-        self._git_commit = None
-        self._git_branch = None
-        self._git_describe = None
-        self._build_platform = []
-
-        self._kernel_image = None
-        self._kernel_config = None
-        self._system_map = None
-        self._text_offset = None
-        self._modules = None
-        self._modules_dir = None
-        self._build_log = None
-        self._dtb_dir = None
-        self._kconfig_fragments = None
-
-        self._version = None
+        self._status = None
+        self.arch = None
+        self.build_log = None
+        self.build_time = 0
+        self.dirname = None
+        self.dtb_dir = None
+        self.errors = 0
+        self.git_branch = None
+        self.git_commit = None
+        self.git_describe = None
+        self.git_url = None
+        self.job_id = None
+        self.kconfig_fragments = None
+        self.kernel_config = None
+        self.kernel_image = None
+        self.modules = None
+        self.modules_dir = None
+        self.system_map = None
+        self.text_offset = None
+        self.warnings = 0
 
     @property
     def collection(self):
@@ -108,20 +105,6 @@ class DefconfigDocument(modb.BaseDocument):
         self._id = value
 
     @property
-    def job_id(self):
-        """The job ID this defconfig belogns to."""
-        return self._job_id
-
-    @job_id.setter
-    def job_id(self, value):
-        """Set the job ID this defconfig belongs to.
-
-        :param value: The job ID.
-        :type value: str
-        """
-        self._job_id = value
-
-    @property
     def job(self):
         """The job this defconfig belongs too."""
         return self._job
@@ -130,6 +113,11 @@ class DefconfigDocument(modb.BaseDocument):
     def kernel(self):
         """The kernel this defconfig was built against."""
         return self._kernel
+
+    @property
+    def defconfig(self):
+        """The defconfig name."""
+        return self._defconfig
 
     @property
     def metadata(self):
@@ -167,81 +155,6 @@ class DefconfigDocument(modb.BaseDocument):
         self._status = value
 
     @property
-    def defconfig(self):
-        """The defconfig name of this document."""
-        return self._defconfig
-
-    @property
-    def errors(self):
-        """Number of errors associated with this defconfig."""
-        return self._errors
-
-    @errors.setter
-    def errors(self, value):
-        """Set the number of errors."""
-        self._errors = value
-
-    @property
-    def warnings(self):
-        """Number of warnings associated with this defconfig."""
-        return self._warnings
-
-    @warnings.setter
-    def warnings(self, value):
-        """Set the number of errors."""
-        self._warnings = value
-
-    @property
-    def arch(self):
-        """The architecture this defconfig has been built with."""
-        return self._arch
-
-    @arch.setter
-    def arch(self, value):
-        """Set the architecture this defconfig has been built with."""
-        self._arch = value
-
-    @property
-    def dirname(self):
-        """The name of the directory this defconfig is stored in."""
-        return self._dirname
-
-    @dirname.setter
-    def dirname(self, value):
-        """Set the name of the directory where this defconfig is stored."""
-        self._dirname = value
-
-    @property
-    def git_url(self):
-        """The git URL where the code comes from."""
-        return self._git_url
-
-    @git_url.setter
-    def git_url(self, value):
-        """Set the git URL of this defconfig document."""
-        self._git_url = value
-
-    @property
-    def git_commit(self):
-        """The git commit SHA."""
-        return self._git_commit
-
-    @git_commit.setter
-    def git_commit(self, value):
-        """Set the git commit SHA."""
-        self._git_commit = value
-
-    @property
-    def git_branch(self):
-        """The branch name of the repository used."""
-        return self._git_branch
-
-    @git_branch.setter
-    def git_branch(self, value):
-        """Set the branch name of the repository used."""
-        self._git_branch = value
-
-    @property
     def build_platform(self):
         """Details about the platform used to build."""
         return self._build_platform
@@ -252,26 +165,6 @@ class DefconfigDocument(modb.BaseDocument):
         if not isinstance(value, types.ListType):
             raise TypeError("Value passed is not a list: %s", type(value))
         self._build_platform = value
-
-    @property
-    def git_describe(self):
-        """The git describe value of the repository."""
-        return self._git_describe
-
-    @git_describe.setter
-    def git_describe(self, value):
-        """Set the git describe value of the repository."""
-        self._git_describe = value
-
-    @property
-    def build_time(self):
-        """How long it took to build this defconfig."""
-        return self._build_time
-
-    @build_time.setter
-    def build_time(self, value):
-        """Set the build time value."""
-        self._build_time = value
 
     @property
     def version(self):
@@ -286,96 +179,6 @@ class DefconfigDocument(modb.BaseDocument):
         :type param: str
         """
         self._version = value
-
-    @property
-    def kernel_config(self):
-        """The kernel config file name."""
-        return self._kernel_config
-
-    @kernel_config.setter
-    def kernel_config(self, value):
-        """Set the kernel config file name."""
-        self._kernel_config = value
-
-    @property
-    def system_map(self):
-        """The system map file name."""
-        return self._system_map
-
-    @system_map.setter
-    def system_map(self, value):
-        """Set the system map file name."""
-        self._system_map = value
-
-    @property
-    def text_offset(self):
-        """The text offset value."""
-        return self._text_offset
-
-    @text_offset.setter
-    def text_offset(self, value):
-        """Set the text offset value."""
-        self._text_offset = value
-
-    @property
-    def modules(self):
-        """The modules file name."""
-        return self._modules
-
-    @modules.setter
-    def modules(self, value):
-        """Set the modules file name."""
-        self._modules = value
-
-    @property
-    def dtb_dir(self):
-        """The dtb directory name."""
-        return self._dtb_dir
-
-    @dtb_dir.setter
-    def dtb_dir(self, value):
-        """Set the dtb directory name."""
-        self._dtb_dir = value
-
-    @property
-    def kernel_image(self):
-        """The kernel image file name."""
-        return self._kernel_image
-
-    @kernel_image.setter
-    def kernel_image(self, value):
-        """Se the kernel image file name."""
-        self._kernel_image = value
-
-    @property
-    def build_log(self):
-        """The build log file name."""
-        return self._build_log
-
-    @build_log.setter
-    def build_log(self, value):
-        """Set the build log file name."""
-        self._build_log = value
-
-    @property
-    def modules_dir(self):
-        """The directory containing the modules."""
-        return self._modules_dir
-
-    @modules_dir.setter
-    def modules_dir(self, value):
-        """Set the modules directory."""
-        self._modules_dir = value
-
-    @property
-    def kconfig_fragments(self):
-        """The config fragment used."""
-        return self._kconfig_fragments
-
-    @kconfig_fragments.setter
-    def kconfig_fragments(self, value):
-        """Set the config fragment used."""
-        self._kconfig_fragments = value
 
     @property
     def defconfig_full(self):
