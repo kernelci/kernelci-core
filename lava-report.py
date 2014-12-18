72f9b11d7f1ed0348c41e054f2989ba2887b9f49
Tyler Baker <tyler.baker@linaro.org>
2014-12-18 14:44:45 -0800

    kernel-ci: remove .dtb from peach-pi dtb

--- lava-report.py
+++ lava-report.py
@@ -13,7 +13,7 @@ log2html = 'https://git.linaro.org/people/kevin.hilman/build-scripts.git/blob_pl
 device_map = {'arndale': 'exynos5250-arndale',
               'snow': 'exynos5250-snow.dtb',
               'arndale-octa': 'exynos5420-arndale-octa',
-              'peach-pi': 'exynos5800-peach-pi.dtb',
+              'peach-pi': 'exynos5800-peach-pi',
               'odroid-xu3': 'exynos5420-smdk5420',
               'odroid-u2': 'exynos4412-odroidu3',
               'beaglebone-black': 'am335x-boneblack',
