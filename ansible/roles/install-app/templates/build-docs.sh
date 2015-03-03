19c7df905705b7ce8d19cdeef9c08766390948ef
Milo Casagrande <milo.casagrande@linaro.org>
2015-03-03 14:38:50 +0100

    ansible: Fix docs build script.
    Change-Id: I4473b936e3b66cd5b8888e5d571f4f4bb05c9404

--- ansible/roles/install-app/templates/build-docs.sh
+++ ansible/roles/install-app/templates/build-docs.sh
@@ -6,10 +6,12 @@ SCHEMA_DIR="$DOC_DIR/schema"
 VENV_DIR="$BASE_PATH/.venv/{{ hostname }}"
 
 . "$VENV_DIR/bin/activate"
-cd $DOC_DIR && make html
+cd $DOC_DIR && make html > /dev/null
 deactivate
 
 cp -ax $BUILD_DIR/* {{ web_root }}/{{ hostname }}
 cp -ax $SCHEMA_DIR {{ web_root }}/{{ hostname }}
 
-exit 1
+chown -R {{ web_user}}:{{ web_user }} {{ web_root }}/{{ hostname }}
+
+exit 0
