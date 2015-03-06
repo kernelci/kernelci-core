a994df0e1f903632ab76840598b4041dca144ce6
Tyler Baker <tyler.baker@linaro.org>
2015-03-05 16:54:15 -0800

    kernel-ci: add mirabox support to lava-report

--- lava-report.py
+++ lava-report.py
@@ -10,7 +10,8 @@ from utils import *
 
 log2html = 'https://git.linaro.org/people/kevin.hilman/build-scripts.git/blob_plain/HEAD:/log2html.py'
 
-device_map = {'arndale': ['exynos5250-arndale', 'exynos'],
+device_map = {'armada-370-mirabox': ['armada-370-mirabox', 'mvebu'],
+              'arndale': ['exynos5250-arndale', 'exynos'],
               'snow': ['exynos5250-snow', 'exynos'],
               'arndale-octa': ['exynos5420-arndale-octa','exynos'],
               'peach-pi': ['exynos5800-peach-pi', 'exynos'],
