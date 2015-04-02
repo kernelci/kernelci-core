8f58c40a9d0bd115bcbbcafddbf6090dfe350c78
Tyler Baker <tyler.baker@linaro.org>
2015-04-02 16:43:23 -0700

    kernel-ci: check that report_directory exists

--- lava-report.py
+++ lava-report.py
@@ -520,11 +520,13 @@ def boot_report(args):
     # sendmail
     if args.email:
         print 'Sending e-mail summary to %s' % args.email
-        cmd = 'cat %s | sendmail -t' % os.path.join(report_directory, boot)
-        subprocess.check_output(cmd, shell=True)
-        if dt_tests:
-            cmd = 'cat %s | sendmail -t' % os.path.join(report_directory, dt_self_test)
+        if os.path.exists(report_directory):
+            cmd = 'cat %s | sendmail -t' % os.path.join(report_directory, boot)
             subprocess.check_output(cmd, shell=True)
+        if dt_tests:
+            if os.path.exists(report_directory):
+                cmd = 'cat %s | sendmail -t' % os.path.join(report_directory, dt_self_test)
+                subprocess.check_output(cmd, shell=True)
 
 def main(args):
     if args.boot:
