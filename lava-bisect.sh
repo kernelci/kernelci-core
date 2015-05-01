17521f80cf5400e58838ee6caf3f2e31b1e17dd5
Tyler Baker <tyler.baker@linaro.org>
2015-04-30 19:37:01 -0700

    kernel-ci: lava-bisect: add very basic bisect script

---
+++ lava-bisect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $WORKSPACE
+build.py -i -p production -c $1
+if [ $? -ne 0 ]; then
+	echo "Failed to build, skipping..."
+	exit 125
+fi
+export GIT_DESCRIBE=$(git describe)
+echo $GIT_DESCRIBE
+cd $WORKSPACE/local/lava-ci/
+python lava-kernel-ci-job-creator.py http://storage.kernelci.org/$TREE_NAME/$GIT_DESCRIBE/$ARCH-$1/ --plans boot --targets $2
+python lava-job-runner.py $3 $4 $5 --stream /anonymous/kernel-ci/ --poll kernel-ci.json --bisect
+if [ $? -ne 0 ]; then
+	echo "Boot failed, git bisect bad"
+	exit 1
+else
+	echo "Boot passed, git bisect good"
+	exit 0
+fi
