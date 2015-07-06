2bc0da8acbc4b205757be27fc96142adbdc7b936
Milo Casagrande <milo.casagrande@linaro.org>
2015-07-06 18:26:15 +0200

    Add new single build and log import functions.

--- app/taskqueue/tasks.py
+++ app/taskqueue/tasks.py
@@ -49,9 +49,31 @@ def import_job(json_obj, db_options, mail_options=None):
     :type mail_options: dictionary
     :return The ID of the job document.
     """
-    job_id, errors = utils.build.import_from_dir(json_obj, db_options)
+    # job_id is necessary since it is injected by Celery into another function.
+    job_id, errors = utils.build.import_multiple_builds(json_obj, db_options)
     # TODO: handle errors.
     return job_id
+
+
+@taskc.app.task(name="import-defconfig")
+def import_build(json_obj, db_options, mail_options=None):
+    """Import a single build document.
+
+    This is used to provide a Celery-task access to the import function.
+
+    :param json_obj: The JSON object with the values necessary to import the
+    build data.
+    :type json_obj: dictionary
+    :param db_options: The database connection options.
+    :type db_options: dictionary
+    :return The build ID and the job ID.
+    """
+    # build_id and job_id are necessary since they are injected by Celery into
+    # another function.
+    build_id, job_id, errors = utils.build.import_single_build(
+        json_obj, db_options)
+    # TODO: handle errors.
+    return build_id, job_id
 
 
 @taskc.app.task(name="parse-build-log")
@@ -71,7 +93,32 @@ def parse_build_log(job_id, json_obj, db_options, mail_options=None):
     :type mail_options: dictionary
     :return A 2-tuple: The status code, and the errors data structure.
     """
-    return utils.log_parser.parse_build_log(job_id, json_obj, db_options)
+    status, errors = utils.log_parser.parse_build_log(
+        job_id, json_obj, db_options)
+    # TODO: handle errors.
+    return status
+
+
+@taskc.app.task(name="parse-single-build-log")
+def parse_single_build_log(
+        prev_res, db_options, mail_options=None):
+    """Wrapper around the real build log parsing function.
+
+    Used to provided a task to the import function.
+
+    :param prev_res: The results of the previous task, that should be a list
+    with two elements: the build ID and the job ID.
+    :type prev_res: list
+    :param db_options: The database connection parameters.
+    :type db_options: dictionary
+    :param mail_options: The options necessary to connect to the SMTP server.
+    :type mail_options: dictionary
+    :return A 2-tuple: The status code, and the errors data structure.
+    """
+    status, errors = utils.log_parser.parse_single_build_log(
+        prev_res[0], prev_res[1], db_options)
+    # TODO: handle errors.
+    return status
 
 
 @taskc.app.task(name="import-boot")
