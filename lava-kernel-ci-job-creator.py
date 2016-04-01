4adfeecb0ec28b73b6fd722948150a76cb9d31ba
Quentin Schulz <quentin.schulz@free-electrons.com>
2016-04-01 14:43:59 +0200

    kernel-ci: add ATMEL AT91SAMA5D36EK
    This adds the ATMEL AT91SAMA5D36EK (at91sama5d36ek) to the possible
    targets and create a new entry for job report.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>

--- lava-kernel-ci-job-creator.py
+++ lava-kernel-ci-job-creator.py
@@ -856,6 +856,14 @@ imx6q_nitrogen6x = {'device_type': 'imx6q-nitrogen6x',
                    'lpae': False,
                    'fastboot': False}
 
+sama5d36ek = {'device_type': 'sama5d36ek',
+                    'templates': ['generic-arm-dtb-kernel-ci-boot-template.json'],
+                    'defconfig_blacklist': ['arm-allmodconfig'],
+                    'kernel_blacklist': [],
+                    'nfs_blacklist': [],
+                    'lpae': False,
+                    'fastboot': False}
+
 sam9261ek = {'device_type': 'at91sam9261ek',
                     'templates': ['generic-arm-dtb-kernel-ci-boot-template.json'],
                     'defconfig_blacklist': ['arm-allmodconfig'],
@@ -936,6 +944,7 @@ device_map = {'bcm2835-rpi-b-plus.dtb': [bcm2835_rpi_b_plus],
               'armada-388-clearfog.dtb': [armada_388_clearfog],
               'sun5i-r8-chip.dtb': [chip],
               'imx6q-nitrogen6x.dtb': [imx6q_nitrogen6x],
+              'sama5d36ek.dtb': [sama5d36ek],
               'at91sam9261ek.dtb': [sam9261ek],
               'at91sam9x25ek.dtb': [sam9x25ek],
               'armada-xp-openblocks-ax3-4.dtb': [openblocks_ax3]}
