c84e540f15e76c8e6341fb86d4e93c7108fb0263
Milo Casagrande <milo.casagrande@linaro.org>
2016-07-06 11:11:49 +0200

    Remove static method on use common key creation one

--- app/handlers/boot_regressions.py
+++ app/handlers/boot_regressions.py
@@ -21,9 +21,8 @@ import models
 import utils.db
 
 from utils.boot.regressions import (
-    REGRESSION_FMT,
-    get_regressions_by_key,
-    sanitize_key
+    create_regressions_key,
+    get_regressions_by_key
 )
 
 
@@ -63,67 +62,45 @@ class BootRegressionsHandler(hbase.BaseHandler):
             response.status_code = 400
             response.reason = "Wrong ID value provided"
         else:
-            response = self.find_regressions(obj_id)
+            response = find_regressions(obj_id, self.db)
 
         return response
 
-    def find_regressions(self, doc_id):
-        """Look for the regressions a boot report.
 
-        :param doc_id: The id of the boot report to look for regressions.
-        :type doc_id: ObjectId
-        :return HandlerResponse A HandlerResponse object.
-        """
-        response = hresponse.HandlerResponse()
-        # First make sure we have a valid boot_id value.
-        boot_doc = utils.db.find_one2(
-            self.db[models.BOOT_COLLECTION], doc_id)
+def find_regressions(doc_id, database):
+    """Look for the regressions of a boot report.
 
-        if boot_doc:
-            regr_idx_doc = utils.db.find_one2(
-                self.db[models.BOOT_REGRESSIONS_BY_BOOT_COLLECTION],
-                {models.BOOT_ID_KEY: doc_id})
+    :param doc_id: The id of the boot report to look for regressions.
+    :type doc_id: ObjectId
+    :return HandlerResponse A HandlerResponse object.
+    """
+    response = hresponse.HandlerResponse()
+    # First make sure we have a valid boot_id value.
+    boot_doc = utils.db.find_one2(
+        database[models.BOOT_COLLECTION], doc_id)
 
-            if regr_idx_doc:
-                spec = {
-                    models.ID_KEY:
-                        regr_idx_doc[models.BOOT_REGRESSIONS_ID_KEY]
-                }
+    if boot_doc:
+        regr_idx_doc = utils.db.find_one2(
+            database[models.BOOT_REGRESSIONS_BY_BOOT_COLLECTION],
+            {models.BOOT_ID_KEY: doc_id})
 
-                result = utils.db.find_one2(
-                    self.collection, spec, fields=[models.REGRESSIONS_KEY])
+        if regr_idx_doc:
+            spec = {
+                models.ID_KEY:
+                    regr_idx_doc[models.BOOT_REGRESSIONS_ID_KEY]
+            }
 
-                if result:
-                    response.result = get_regressions_by_key(
-                        self.generate_regression_key(boot_doc),
-                        result[models.REGRESSIONS_KEY])
-                    response.count = len(response.result)
-        else:
-            response.status_code = 404
-            response.reason = "Resource '{:s}' not found".format(str(doc_id))
+            result = utils.db.find_one2(
+                database[models.BOOT_REGRESSIONS_COLLECTION],
+                spec, fields=[models.REGRESSIONS_KEY])
 
-        return response
+            if result:
+                response.result = get_regressions_by_key(
+                    create_regressions_key(boot_doc),
+                    result[models.REGRESSIONS_KEY])
+                response.count = len(response.result)
+    else:
+        response.status_code = 404
+        response.reason = "Resource '{:s}' not found".format(str(doc_id))
 
-    @staticmethod
-    def generate_regression_key(boot_doc):
-        """Generate the regressions key for this boot report.
-
-        The key is used to lookup the boot report regressions in the
-        regressions data structure.
-
-        :param boot_doc: The boot report document.
-        :type boot_doc: dict
-        :return str The boot regression key.
-        """
-        b_get = boot_doc.get
-
-        arch = b_get(models.ARCHITECTURE_KEY)
-        b_instance = \
-            sanitize_key(str(b_get(models.BOARD_INSTANCE_KEY)).lower())
-        board = sanitize_key(b_get(models.BOARD_KEY))
-        compiler = sanitize_key(str(b_get(models.COMPILER_VERSION_EXT_KEY)))
-        defconfig = sanitize_key(b_get(models.DEFCONFIG_FULL_KEY))
-        lab = b_get(models.LAB_NAME_KEY)
-
-        return REGRESSION_FMT.format(
-            lab, arch, board, b_instance, defconfig, compiler)
+    return response
