5ab9fc953bf7d1670023a76836166e118e718d56
Kevin Hilman <khilman@baylibre.com>
2016-09-09 10:19:45 -0700

    build.py: MIPS: use prefix for CodeSourcery toolchain

--- build.py
+++ build.py
@@ -24,7 +24,7 @@ from urlparse import urljoin
 cross_compilers = {
     "arm": "arm-linux-gnueabihf-",
     "arm64": "aarch64-linux-gnu-",
-    "mips": "mips-img-linux-gnu-",
+    "mips": "mips-linux-gnu-",
     "i386": None,
     "x86": None,
     "x86_64": None,
