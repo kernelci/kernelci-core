d0c41ec2b7228265cb5d08d76792ffe995fff7e2
Milo Casagrande <milo.casagrande@linaro.org>
2017-03-28 10:04:04 +0200

    Fix boot report summary URL

--- app/utils/report/boot.py
+++ app/utils/report/boot.py
@@ -45,7 +45,8 @@ BOARD_URL = (
     u"{base_url:s}/boot/{board:s}/job/{job:s}/branch/{git_branch:s}"
     u"/kernel/{kernel:s}/defconfig/{defconfig:s}/"
 )
-BOOT_SUMMARY_URL = u"{boot_url:s}/{job:s}/kernel/{kernel:s}/"
+BOOT_SUMMARY_URL = \
+    u"{boot_url:s}/{job:s}/branch/{git_branch:s}/kernel/{kernel:s}/"
 BOOT_REGRESSIONS_URL = BOOT_SUMMARY_URL + u"#regressions"
 BUILD_SUMMARY_URL = \
     u"{build_url:s}/{job:s}/branch/{git_branch:s}/kernel/{kernel:s}/"
