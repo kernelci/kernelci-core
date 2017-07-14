a99739b906b84f5d41ed8d0902bf20071d254991
Matt Hart <matthew.hart@linaro.org>
2017-07-14 12:49:41 +0100

    lava-v2: try and fetch a defconfig count

--- jenkins/lava-boot-v2.sh
+++ jenkins/lava-boot-v2.sh
@@ -2,6 +2,13 @@
 
 set -x
 
+curl --silent --fail ${STORAGE}/${tree_name}/${branch}/${GIT_DESCRIBE}/${ARCH}_defconfig.count > ${ARCH}_defconfig.count
+if [ $? == 0 ]; then
+	DEFCONFIG_COUNT=`cat ${ARCH}_defconfig.count`
+else
+	DEFCONFIG_COUNT=0
+fi
+
 if [ ${LAB} = "lab-tbaker" ] || [ ${LAB} = "lab-tbaker-dev" ]; then
 	python lava-v2-jobs-from-api.py --callback ${CALLBACK} --api ${API} --storage ${STORAGE} --lab ${LAB} --describe ${GIT_DESCRIBE} --tree ${TREE} --branch ${BRANCH} --arch ${ARCH} --plans boot boot-be boot-kvm boot-kvm-uefi boot-nfs boot-nfs-mp --token ${API_TOKEN}
     python lava-v2-submit-jobs.py --username kernel-ci --jobs ${LAB} --token ${LAVA_TBAKER_TOKEN} --server http://lava.kernelci.org/RPC2/
@@ -20,4 +27,4 @@ elif [ ${LAB} = "lab-baylibre-seattle" ]; then
 elif [ ${LAB} = "lab-free-electrons" ] || [ ${LAB} = "lab-free-electrons-dev" ]; then
 	python lava-v2-jobs-from-api.py --callback ${CALLBACK} --api ${API} --storage ${STORAGE} --lab ${LAB} --describe ${GIT_DESCRIBE} --tree ${TREE} --branch ${BRANCH} --arch ${ARCH} --plans  boot boot-be boot-kvm boot-kvm-uefi boot-nfs boot-nfs-mp --token ${API_TOKEN} --priority medium
     python lava-v2-submit-jobs.py --username kernel-ci --jobs ${LAB} --token ${LAVA_FREE_ELECTRONS_TOKEN} --server https://lab.free-electrons.com/RPC2/
-fi    
+fi
