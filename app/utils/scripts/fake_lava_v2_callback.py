948bb7c294e1cf4a4730fbdf8f586aea7adef38d
Loys Ollivier <lollivier@baylibre.com>
2017-12-19 17:13:02 +0100

    utils/scripts: fake_lava_v2_callback
    Add a script that takes as input a raw json file and sends a callback
    event to a kernelci backend instance. This script is used for
    development and debug.
    
    In order to use the script, the user must have a json callback file from
    lava v2 available locally.
    Such file can be found on a server running kernelci backend in the
    utils.BASEPATH/json-raw/ folder.
    
    Once this PR is merged those files will also be available on
    "https://storage.kernelci.org/json-raw/"
    
    Signed-off-by: Loys Ollivier <lollivier@baylibre.com>

---
+++ app/utils/scripts/fake_lava_v2_callback.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import requests
+import sys
+import json
+import argparse
+
+
+def fake_callback(backend_url, lab_name, lab_token, filename):
+    headers = {
+        "Authorization": lab_token
+    }
+    print "Looking for file {}".format(filename)
+    try:
+        json_file = open(filename)
+    except (IOError) as err:
+        print "Can't find file, error: {}".format(err)
+        return
+    payload = json.load(json_file)
+    url = backend_url + "/callback/lava/boot?lab_name=" + lab_name + "&status=2&status_string=complete"
+    response = requests.post(url, headers=headers, json=payload)
+    print response
+
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser(
+        description="Resend a LAVA v2 callback event from a JSON file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--url', action='store',
+                        help="The backend url to send callbacks to",
+                        required=True)
+    parser.add_argument('--lab-name', action='store',
+                        help="The lab-name as registered in the backend.",
+                        required=True)
+    parser.add_argument('--token', action='store',
+                        help="The token associated to the lab-name",
+                        required=True)
+    parser.add_argument('--filename', action='store',
+                        help="The json file that contains the callback data.",
+                        required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_cmdline()
+    fake_callback(args.url, args.lab_name, args.token, args.filename)
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
