707224ccc4d0b310969f1607a188d314bd647de2
Guillaume Tucker <guillaume.tucker@collabora.com>
2018-06-01 15:23:41 +0100

    lava-boot-v2: also run on lab-pengutronix-dev
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- jenkins/lava-boot-v2.sh
+++ jenkins/lava-boot-v2.sh
@@ -45,7 +45,7 @@ elif [ ${LAB} = "lab-embeddedbits" ]; then
 elif [ ${LAB} = "lab-jsmoeller" ]; then
   python lava-v2-jobs-from-api.py --defconfigs ${DEFCONFIG_COUNT} --callback ${CALLBACK} --api ${API} --storage ${STORAGE} --lab ${LAB} --describe ${GIT_DESCRIBE} --tree ${TREE} --branch ${BRANCH} --arch ${ARCH} --plans boot boot-kvm boot-kvm-uefi boot-nfs boot-nfs-mp --token ${API_TOKEN}
   python lava-v2-submit-jobs.py --username kernel-ci --jobs ${LAB} --token ${LAVA_JSMOELLER_TOKEN} --lab ${LAB}
-elif [ ${LAB} = "lab-pengutronix" ]; then
+elif [ ${LAB} = "lab-pengutronix" ] || [ ${LAB} = "lab-pengutronix-dev" ]; then
   python lava-v2-jobs-from-api.py --defconfigs ${DEFCONFIG_COUNT} --callback ${CALLBACK} --api ${API} --storage ${STORAGE} --lab ${LAB} --describe ${GIT_DESCRIBE} --tree ${TREE} --branch ${BRANCH} --arch ${ARCH} --plans boot boot-kvm boot-kvm-uefi boot-nfs boot-nfs-mp --token ${API_TOKEN}
   python lava-v2-submit-jobs.py --username kernel-ci --jobs ${LAB} --token ${LAVA_PENGUTRONIX_TOKEN} --lab ${LAB}
 elif [ ${LAB} = "lab-linaro-lkft" ] || [ ${LAB} = "lab-linaro-lkft-dev" ]; then
