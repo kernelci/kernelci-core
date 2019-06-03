ecd3564304907fc82501792643deae18ac9f9537
Kevin Hilman <khilman@baylibre.com>
2019-06-03 12:19:28 -0700

    jenkins/buildroot-builder: add fragment
    Support addition fragments when building.  This will initially be used
    to build the additional "baseline" fragment for bootrr.
    
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- jenkins/buildroot-builder.sh
+++ jenkins/buildroot-builder.sh
@@ -2,10 +2,10 @@
 env
 
 # Build 
-./configs/frags/build ${arch}
+./configs/frags/build ${arch} ${frag}
 
 # Publish
 set -x
-PUBLISH_PATH=images/rootfs/buildroot/$(git describe)/${arch}/base
+PUBLISH_PATH=images/rootfs/buildroot/$(git describe)/${arch}/${frag}
 ls -l output/images
 (cd output/images; push-source.py --token ${API_TOKEN} --api ${API} --publish_path ${PUBLISH_PATH} --file *)
