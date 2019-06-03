d73b52dc57eb36d3d94a2063b51cf60925e0221e
Guillaume Tucker <guillaume.tucker@collabora.com>
2019-06-03 17:09:41 +0100

    buildroot-builder.sh: add arch and frag arguments
    Add arch and frag arguments on the command line rather than relying on
    the environment variables.  Default to "base" for the fragment as this
    is how KernelCI's buildroot build script works.  Also use the fragment
    name in the path when uploading the rootfs binaries to the storage
    server.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- jenkins/buildroot-builder.sh
+++ jenkins/buildroot-builder.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 env
 
-# Build 
-./configs/frags/build ${arch}
+arch=${1}
+frag=${2}
+variant=${frag:-base}
+
+# "base" is always used by default in the build script
+[ $frag == "base" ] && frag=""
+
+# Build
+./configs/frags/build ${arch} ${frag}
 
 # Publish
 set -x
-PUBLISH_PATH=images/rootfs/buildroot/$(git describe)/${arch}/base
+PUBLISH_PATH=images/rootfs/buildroot/$(git describe)/${arch}/${variant}
 ls -l output/images
 (cd output/images; push-source.py --token ${API_TOKEN} --api ${API} --publish_path ${PUBLISH_PATH} --file *)
