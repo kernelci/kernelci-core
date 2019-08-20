247dc55c24d4362ace5a5dff232189bb67bf11ed
Guillaume Tucker <guillaume.tucker@collabora.com>
2019-08-20 22:21:31 +0100

    add kernelci.config.lab to have lab configs in YAML
    Add kernelci.config.lab module to handle test lab configuration.  This
    is to include all the common paramters of test labs such as their API
    URL, filters to not run some tests and the type of each lab (typically
    LAVA but others can be defined).
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ kernelci/config/lab.py
@@ -0,0 +1,101 @@
+# Copyright (C) 2019 Collabora Limited
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+#
+# This module is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import yaml
+
+from kernelci.config import FilterFactory, YAMLObject
+
+
+class Lab(YAMLObject):
+    """Test lab"""
+
+    def __init__(self, name, lab_type, url, filters=None):
+        self._name = name
+        self._lab_type = lab_type
+        self._url = url
+        self._filters = filters or list()
+
+    @classmethod
+    def from_yaml(cls, lab, kw):
+        return cls(**kw)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def lab_type(self):
+        return self._lab_type
+
+    @property
+    def url(self):
+        return self._url
+
+    def match(self, data):
+        return all(f.match(**data) for f in self._filters)
+
+
+class Lab_LAVA(Lab):
+
+    def __init__(self, priority='medium', *args, **kwargs):
+        super(Lab_LAVA, self).__init__(*args, **kwargs)
+        self._priority = priority
+
+    @property
+    def priority(self):
+        return self._priority
+
+    @classmethod
+    def from_yaml(cls, lab, kw):
+        priority = lab.get('priority')
+        if priority:
+            kw['priority'] = priority
+        return cls(**kw)
+
+
+class LabFactory(YAMLObject):
+    _lab_types = {
+        'lava': Lab_LAVA,
+    }
+
+    @classmethod
+    def from_yaml(cls, name, lab):
+        lab_type = lab.get('lab_type')
+        kw = cls._kw_from_yaml(lab, ['url'])
+        kw.update({
+            'name': name,
+            'lab_type': lab_type,
+            'filters': FilterFactory.from_data(lab),
+        })
+        lab_cls = cls._lab_types[lab_type] if lab_type else Lab
+        return lab_cls.from_yaml(lab, kw)
+
+
+def from_yaml(yaml_path):
+    with open(yaml_path) as f:
+        data = yaml.load(f)
+
+    labs = {
+        name: LabFactory.from_yaml(name, lab)
+        for name, lab in data['labs'].iteritems()
+    }
+
+    config_data = {
+        'labs': labs,
+    }
+
+    return config_data
