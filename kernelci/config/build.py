3b40138f4f4e10887cfc2b70ed4a5c56c84a1bab
Michal Galka <michal.galka@collabora.com>
2019-09-16 04:14:49 +0200

    kci_build: Add get_reference subcommand
    get_reference subcommand of kci_build prints reference tree and branch
    for bisections. These are taken from the reference section of build
    config or from reference_defaults when not found in the build config.
    
    Signed-off-by: Michal Galka <michal.galka@collabora.com>

--- kernelci/config/build.py
+++ kernelci/config/build.py
@@ -49,6 +49,33 @@ class Tree(YAMLObject):
         return self._url
 
 
+class Reference(YAMLObject):
+    """Kernel reference tree and branch model."""
+
+    def __init__(self, tree, branch):
+        """Reference is a tree and branch used for bisections
+
+        *tree* is a Tree object
+        *branch* is the branch name to be used from the tree
+        """
+        self._tree = tree
+        self._branch = branch
+
+    @classmethod
+    def from_yaml(cls, reference, trees):
+        kw = cls._kw_from_yaml(reference, ['tree', 'branch'])
+        kw['tree'] = trees[kw['tree']]
+        return cls(**kw)
+
+    @property
+    def tree(self):
+        return self._tree
+
+    @property
+    def branch(self):
+        return self._branch
+
+
 class Fragment(YAMLObject):
     """Kernel config fragment model."""
 
@@ -290,7 +317,7 @@ class BuildVariant(YAMLObject):
 class BuildConfig(YAMLObject):
     """Build configuration model."""
 
-    def __init__(self, name, tree, branch, variants):
+    def __init__(self, name, tree, branch, variants, reference):
         """A build configuration defines the actual kernels to be built.
 
         *name* is the name of the build configuration.  It is arbitrary and
@@ -304,11 +331,16 @@ class BuildConfig(YAMLObject):
 
         *variants* is a list of BuildVariant objects, to define all the
                    variants to build for this tree / branch combination.
+
+        *reference* is a Reference object which defines the tree and branch
+                    for bisections when no base commit is found for
+                    the good and bad revisions.
         """
         self._name = name
         self._tree = tree
         self._branch = branch
         self._variants = variants
+        self._reference = reference
 
     @classmethod
     def from_yaml(cls, config, name, trees, fragments, build_envs, defaults):
@@ -325,6 +357,8 @@ class BuildConfig(YAMLObject):
             for name, variant in config_variants.iteritems()
         ]
         kw['variants'] = {v.name: v for v in variants}
+        reference = config.get('reference', defaults['reference'])
+        kw['reference'] = Reference.from_yaml(reference, trees)
         return cls(**kw)
 
     @property
@@ -345,6 +379,10 @@ class BuildConfig(YAMLObject):
 
     def get_variant(self, name):
         return self._variants[name]
+
+    @property
+    def reference(self):
+        return self._reference
 
 
 def from_yaml(yaml_path):
