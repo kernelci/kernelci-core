e4ee955cd3afa0df90d9f987d0346a2737d35077
Lakshmipathi Ganapathi <lakshmipathi.ganapathi@collabora.com>
2020-01-13 21:27:47 +0530

    Add kci_rootfs upload option
    Upload rootfs images to kernelci backend.
    Usage: ./kci_rootfs upload --rootfs-dir=/path/to/rootfsdir
           --upload-path=path/to/uploaddir --api=ip-address --token <token>
    
    Signed-off-by: Lakshmipathi Ganapathi <lakshmipathi.ganapathi@collabora.com>

---
+++ kernelci/storage.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 Collabora Limited
+# Author: Lakshmipathi Ganapathi <lakshmipathi.ganapathi@collabora.com>
+#
+# This module is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import os
+import requests
+from urllib.parse import urljoin
+from kernelci import shell_cmd
+
+
+def upload_files(api, token, path, input_files):
+    """Upload rootfs to KernelCI backend.
+
+    *api* is the URL of the KernelCI backend API
+    *token* is the backend API token to use
+    *path* is the target on KernelCI backend
+    *input_files* dictionary of input files
+    """
+    headers = {
+        'Authorization': token,
+    }
+    data = {
+        'path': path,
+    }
+    files = {
+        'file{}'.format(i): (name, fobj)
+        for i, (name, fobj) in enumerate(input_files.items())
+    }
+    url = urljoin(api, 'upload')
+    resp = requests.post(url, headers=headers, data=data, files=files)
+    resp.raise_for_status()
