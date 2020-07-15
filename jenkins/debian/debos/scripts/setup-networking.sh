31df94d01904e7e6b8d4d9c54ade80a2cc9e2675
Kevin Hilman <khilman@baylibre.com>
2020-07-15 14:40:27 -0700

    rootfs-config: buster: add DHCP client to full rootfs
    Add a DHCP client to the full rootfs (removed for the minimal rootfs).
    
    This is required for proper network setup in a NFSroot setup, since
    debian systemd does not properly setup DNS when the kernel has been
    booted with kernel IP autoconfig (e.g. booting the kernel with
    "ip=dhcp" on the command-line.)
    
    The solution is to run "dhclient" immediately after login
    to (re)configure networking before DNS is usable.
    
    While here, also add some backup, static DNS servers so there
    are some working DNS settings even before things are (re)configured.
    
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- jenkins/debian/debos/scripts/setup-networking.sh
+++ jenkins/debian/debos/scripts/setup-networking.sh
@@ -10,3 +10,7 @@ systemctl enable systemd-resolved
 systemctl enable systemd-timesyncd
 
 ln -sf /lib/systemd/resolv.conf /etc/resolv.conf
+
+# Setup some static DNS defaults (Google Public)
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
