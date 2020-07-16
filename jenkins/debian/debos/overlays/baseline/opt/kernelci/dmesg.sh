9b69ba3c84f731be1683429f34b631d09ccb75fa
Kevin Hilman <khilman@baylibre.com>
2020-07-16 10:50:57 -0700

    rootfs-configs: add baseline overlay to default buster
    Add a minimal overlay for the baseline test-plans to the default
    buster rootfs so it can be used in baseline and baseline-nfs
    test plans.
    
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

---
+++ jenkins/debian/debos/overlays/baseline/opt/kernelci/dmesg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+if [ "$KERNELCI_LAVA" = "y" ]; then
+    alias test-result='lava-test-case'
+else
+    alias test-result='echo'
+fi
+
+for level in crit alert emerg; do
+    dmesg --level=$level --notime -x -k > dmesg.$level
+    test -s dmesg.$level && res=fail || res=pass
+    count=$(cat dmesg.$level | wc -l)
+    cat dmesg.$level
+    test-result \
+        $level \
+        --result $res \
+        --measurement $count \
+        --units lines
+done
+
+exit 0
