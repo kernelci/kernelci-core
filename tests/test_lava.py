f37c61959f74946e33f129ab549cdec1b94309eb
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-07-16 07:43:34 +0100

    tests: run as Python 3 and fix tests.test_lava
    Run the unit tests as Python 3 now that the code base has been ported.
    Also include the correct module kernelci.lab.lava in tests.test_lava
    and adjust the list of devices accordingly since the code has changed
    since then.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- tests/test_lava.py
+++ tests/test_lava.py
@@ -1,22 +1,18 @@
-import kernelci.lava
+import kernelci.lab.lava as lava
 import pytest
 
 
 def test_get_device_type_by_name_1():
-    assert kernelci.lava.get_device_type_by_name("some_board", []) is None
+    assert lava.get_device_type_by_name("some_board", []) is None
 
 
 def test_get_device_type_by_name_2():
-    assert kernelci.lava.get_device_type_by_name("some_board", [], []) is None
+    assert lava.get_device_type_by_name("some_board", [], []) is None
 
 
 @pytest.fixture
 def device_types():
-    device_types = [
-        {"busy": 1, "idle": 0, "name": "x15", "offline": 0},
-        {"busy": 1, "idle": 0, "name": "beaglebone-black", "offline": 0},
-    ]
-    return device_types
+    return ["x15", "beaglebone-black"]
 
 
 @pytest.fixture
@@ -27,21 +23,21 @@ def aliases():
 
 def test_get_device_type_by_name_3(device_types, aliases):
     assert (
-        kernelci.lava.get_device_type_by_name("some_board", device_types,
+        lava.get_device_type_by_name("some_board", device_types,
                                               aliases) is None
     )
 
 
 def test_get_device_type_by_name_4(device_types, aliases):
     assert (
-        kernelci.lava.get_device_type_by_name("x15", device_types, aliases)
+        lava.get_device_type_by_name("x15", device_types, aliases)
         == device_types[0]
     )
 
 
 def test_get_device_type_by_name_5(device_types, aliases):
     assert (
-        kernelci.lava.get_device_type_by_name(
+        lava.get_device_type_by_name(
             "am57xx-beagle-x15", device_types, aliases
         )
         == device_types[0]
@@ -50,6 +46,6 @@ def test_get_device_type_by_name_5(device_types, aliases):
 
 def test_get_device_type_by_name_6(device_types, aliases):
     assert (
-        kernelci.lava.get_device_type_by_name("beaglebone-black", device_types)
+        lava.get_device_type_by_name("beaglebone-black", device_types)
         == device_types[1]
     )
