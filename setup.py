71a104668dcf4fd53085e0a32ca169fb6ba562a2
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-08-21 22:14:20 +0100

    setup.py: bump version to 1.1
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import setuptools
 
 setuptools.setup(
     name='kernelci',
-    version='1.0',
+    version='1.1',
     description="KernelCI core functions",
     author="kernelci.org",
     author_email="kernelci@groups.io",
