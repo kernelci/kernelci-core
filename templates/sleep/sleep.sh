d238683b0e0c95c8c0abe7ae4967ebccc3f81eed
Khouloud Touil <ktouil@baylibre.com>
2020-08-24 11:53:20 +0200

    Costomize the sleep test
    Costomize the sleep test to use parameters and run the suited tests.
    The parameters to be added in the test-config.yaml file to each board
    that supports the test.
    Add a check for the existence of the rtc otherwise it won't execute
    the test.
    
    Signed-off-by: Khouloud Touil <ktouil@baylibre.com>

---
+++ templates/sleep/sleep.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+modes=$*
+ls -l /dev/rtc0
+if [ $? -eq 0 ];then
+	lava-test-case rtc-exist --result pass
+	for mode in $modes; do
+		for i in $(seq 1 10); do
+			rtcwake -d rtc0 -m $mode -s 5
+        	if [ $? -eq 0 ] && result="pass" || result="fail" ;then
+				lava-test-case rtcwake-$mode-$i --result $result
+			fi 
+		done
+	done
+else
+	lava-test-case rtc-exist --result fail
+	echo "No real time clock found !"
+fi
+
