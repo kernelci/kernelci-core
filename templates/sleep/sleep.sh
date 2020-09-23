cbcfc8f3087faef98784ebd417432015a50bd982
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-09-23 08:55:45 +0100

    sleep: fix intendation and white space
    Fix indentation in sleep.sh, remove trailing whitespace and undo blank
    line removal in test-configs.yaml.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>
    Fixes: a72643e2d1e7 ("Costomize the sleep test")

--- templates/sleep/sleep.sh
+++ templates/sleep/sleep.sh
@@ -7,13 +7,12 @@ if [ $? -eq 0 ];then
 	for mode in $modes; do
 		for i in $(seq 1 10); do
 			rtcwake -d rtc0 -m $mode -s 5
-        	if [ $? -eq 0 ] && result="pass" || result="fail" ;then
+		if [ $? -eq 0 ] && result="pass" || result="fail" ;then
 				lava-test-case rtcwake-$mode-$i --result $result
-			fi 
+			fi
 		done
 	done
 else
 	lava-test-case rtc-exist --result fail
 	echo "No real time clock found !"
 fi
-
