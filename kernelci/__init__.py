26979c2f515be66d06c73711e6d7734010c2e1ee
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-11-09 11:52:31 +0000

    kernelci: add sort_check() to check alphabetical order in config
    Add kernelci.sort_check() to help validate the YAML configuration and
    detect when entries are not in alphabetical order.  This will deal
    with numeric fields of varying length, so that xyz-234 comes before
    xyz-1234.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/__init__.py
+++ kernelci/__init__.py
@@ -15,6 +15,7 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import re
 import subprocess
 import sys
 
@@ -29,3 +30,22 @@ def shell_cmd(cmd, ret_code=False):
 def print_flush(msg):
     print(msg)
     sys.stdout.flush()
+
+
+def sort_check(keys):
+    parsed_keys = list((tuple(re.split(r'-|_|\.', key)), key) for key in keys)
+    keys_map = dict(parsed_keys)
+    split_keys = list(k[0] for k in parsed_keys)
+    numeric_keys = []
+    for split_key in split_keys:
+        numeric_keys.extend(k for k in split_key if k.isdigit())
+    max_digits = max(len(k) for k in numeric_keys) if numeric_keys else 0
+    fmt = '{{:0{}d}}'.format(max_digits)
+    sorted_keys = sorted(
+        split_keys,
+        key=lambda x: list(fmt.format(int(k)) if k.isdigit() else k for k in x)
+    )
+    for key_raw, key_sorted in zip(split_keys, sorted_keys):
+        if key_raw != key_sorted:
+            return keys_map[key_raw], keys_map[key_sorted]
+    return None
