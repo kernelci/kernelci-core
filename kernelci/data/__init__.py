140a6a61b11dcc978dc4e0a123cf89580015cc0e
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-03-02 19:57:35 +0000

    kernelci.data: remove hard-coded '/test' path in API URLs
    Remove the hardcoded '/test' path in the database API URLs as kci_data
    is intended to be used to send any kind of data, not just test
    results.  Update kci_data and db-configs.yaml accordingly.  Also
    update docstrings and copyright notices.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/data/__init__.py
+++ kernelci/data/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2020 Collabora Limited
 # Author: Michal Galka <michal.galka@collabora.com>
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 #
 # This module is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
@@ -19,16 +20,35 @@ import importlib
 
 
 class Database:
+    """KernelCI database interface"""
 
     def __init__(self, config, token=None):
+        """Handle KernelCI data and communicate with any supported database
+
+        This abstract class provides an interface for exchanging data with a
+        database using an arbitrary implementation.  It also provides common
+        logic when applicable, to handle KernelCI data in a generic way.
+
+        *config* is the database configuration object
+        *token* is an authentication token for the database
+        """
         self._config = config
         self._token = token
 
-    def submit(self, data):
+    def submit(self, data, path=None, verbose=False):
+        """Submit arbitrary data to the database
+
+        Primitive method to send some data to the database.
+
+        *data* is the payload to send to the database
+        *path* is to identify the part of the API to use to send the data
+        *verbose* is to print more information
+        """
         raise NotImplementedError("Database.submit() must be implemented")
 
     @property
     def config(self):
+        """Database configuration"""
         return self._config
 
 
