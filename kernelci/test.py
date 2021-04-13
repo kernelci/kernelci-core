0399c5ef16e44d5273283609f87e3aef2d784fcf
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-04-13 22:31:35 +0100

    kernelci.test, kci_test: use Metadata class
    Use the new kenelci.build.Metadata class instead of MetaStep to get
    the build meta-data.  Use the new kernelci.build.Step.get_install_path
    method to get the kernel installation path using the conventional
    default values.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/test.py
+++ kernelci/test.py
@@ -19,27 +19,32 @@ import os
 import urllib.parse
 
 
-def match_configs(configs, bmeta, dtbs, lab):
+def match_configs(configs, meta, lab):
     """Filter the test configs for a given kernel build and lab.
 
     *configs* is a list of all the initial test configs
-    *bmeta* is a dictionary with the kernel build meta-data
+    *meta* is a MetaStep object
     *dtbs* is the list of dtb files included in the kernel build
     *lab* is a Lab object instance
 
     The returned value is a list with a subset of the configs that match the
     provided kernel build meta-data and lab filters.
     """
-    defconfig = bmeta['defconfig_full']
-    arch = bmeta['arch']
+    dtbs = meta.get_single_artifact('dtbs', attr='contents')
+    bmeta = meta.get('bmeta')
+    env, kernel, rev = (bmeta.get(key) for key in [
+        'environment', 'kernel', 'revision'
+    ])
+    defconfig = kernel['defconfig_full']
+    arch = env['arch']
 
     filters = {
         'arch': arch,
         'defconfig': defconfig,
-        'kernel': bmeta['git_describe'],
-        'build_environment': bmeta['build_environment'],
-        'tree': bmeta['job'],
-        'branch': bmeta['git_branch'],
+        'kernel': rev['describe'],
+        'build_environment': env['name'],
+        'tree': rev['tree'],
+        'branch': rev['branch'],
     }
 
     flags = {
@@ -65,44 +70,45 @@ def match_configs(configs, bmeta, dtbs, lab):
     return match
 
 
-def get_params(bmeta, target, plan_config, storage):
+def get_params(meta, target, plan_config, storage):
     """Get a dictionary with all the test parameters to run a test job
 
-    *bmeta* is a dictionary with the kernel build meta-data
+    *meta* is a MetaStep object
     *target* is the name of the target platform to run the test
     *plan_config* is a TestPlan object for the test plan to run
     *storage* is the URL of the storage server
     """
+    kernel, rev = (meta.get('bmeta', key) for key in ['kernel', 'revision'])
     arch = target.arch
     dtb = dtb_full = target.dtb
-    dtb_dir = bmeta.get("dtb_dir", "")
     if dtb:
+        dtb_dir = meta.get_single_artifact('dtbs', attr='path')
         dtb = dtb_full = os.path.join(dtb_dir, target.dtb)
         dtb = os.path.basename(dtb)  # hack for dtbs in subfolders
-    file_server_resource = bmeta['file_server_resource']
-    job_px = file_server_resource.replace('/', '-')
-    url_px = file_server_resource
+    publish_path = kernel['publish_path']
+    job_px = publish_path.replace('/', '-')
+    url_px = publish_path
     job_name = '-'.join([job_px, dtb or 'no-dtb',
                          target.name, plan_config.name])
     base_url = urllib.parse.urljoin(storage, '/'.join([url_px, '']))
-    kernel_img = bmeta['kernel_image']
+    kernel_img = meta.get_single_artifact('kernel', 'image', 'path')
     kernel_url = urllib.parse.urljoin(storage, '/'.join([url_px, kernel_img]))
     if dtb_full and dtb_full.endswith('.dtb'):
         dtb_url = urllib.parse.urljoin(
             storage, '/'.join([url_px, dtb_full]))
     else:
         dtb_url = None
-    modules = bmeta.get('modules')
+    modules = meta.get_single_artifact('modules', attr='path')
     modules_url = (
         urllib.parse.urljoin(storage, '/'.join([url_px, modules]))
         if modules else None
     )
     rootfs = plan_config.rootfs
-    defconfig_full = bmeta['defconfig_full']
+    defconfig_full = kernel['defconfig_full']
     defconfig = ''.join(defconfig_full.split('+')[:1])
     endian = 'big' if 'BIG_ENDIAN' in defconfig_full else 'little'
-    describe = bmeta['git_describe']
-    kselftests = bmeta.get('kselftests')
+    describe = rev['describe']
+    kselftests = meta.get_single_artifact('kselftest', attr='path')
     kselftests_url = (
         urllib.parse.urljoin(storage, '/'.join([url_px, kselftests]))
         if kselftests else None
@@ -121,7 +127,7 @@ def get_params(bmeta, target, plan_config, storage):
         'plan': plan_config.base_name,
         'plan_variant': plan_config.name,
         'kernel': describe,
-        'tree': bmeta['job'],
+        'tree': rev['tree'],
         'defconfig': defconfig,
         'defconfig_full': defconfig_full,
         'fastboot': str(target.get_flag('fastboot')).lower(),
@@ -130,17 +136,17 @@ def get_params(bmeta, target, plan_config, storage):
         'base_url': base_url,
         'endian': endian,
         'arch': arch,
-        'git_branch': bmeta['git_branch'],
-        'git_commit': bmeta['git_commit'],
+        'git_branch': rev['branch'],
+        'git_commit': rev['commit'],
         'git_describe': describe,
-        'git_url': bmeta['git_url'],
+        'git_url': rev['url'],
         'initrd_url': rootfs.get_url('ramdisk', arch, endian),
-        'kernel_image': kernel_img,
+        'kernel_image': os.path.basename(kernel_img),
         'nfsrootfs_url': rootfs.get_url('nfs', arch, endian),
         'context': target.context,
         'rootfs_prompt': rootfs.prompt,
-        'file_server_resource': file_server_resource,
-        'build_environment': bmeta['build_environment'],
+        'file_server_resource': publish_path,
+        'build_environment': meta.get('bmeta', 'environment', 'name'),
         'kselftests_url': kselftests_url,
     }
 
