d99513fca7ace6fadb6bf7933e4161ebe35e612b
Corentin Labbe <clabbe@baylibre.com>
2021-09-04 18:41:48 +0000

    kernelci.build: Detect better the endianess of a build
    The current way to detect endianess is to check if a BIG ENDIAN defconfig frag was added.
    But this is wrong for defconfig which is are nativly big endian like arm
    ixp4xx_defconfig.
    
    So the ultimate way to know the endianess is to grep the final .config.
    
    Update kernelci.test accordingly to use the produced meta-data value.
    
    Signed-off-by: Corentin Labbe <clabbe@baylibre.com>
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/test.py
+++ kernelci/test.py
@@ -115,7 +115,7 @@ def get_params(meta, target, plan_config, storage):
     modules_compression = _get_compression(modules_url)
     defconfig_full = kernel['defconfig_full']
     defconfig = ''.join(defconfig_full.split('+')[:1])
-    endian = 'big' if 'BIG_ENDIAN' in defconfig_full else 'little'
+    endian = kernel["endianness"]
     describe = rev['describe']
     kselftests = meta.get_single_artifact('kselftest', attr='path')
     kselftests_url = (
