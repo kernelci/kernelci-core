a91077ea05605a1bf4b030e8d5185a2484c9b3a5
Adrian Ratiu <adrian.ratiu@collabora.com>
2021-09-23 02:36:23 +0300

    rootfs: add firmware install step
    The new step 'extra_firmware' allows a subset of files from the
    linux-firmware repo to be deployed to images, after package
    installs and prior to packing or cpio creation.
    
    This commit just adds the mechanism, individual firmware files
    should be justified and added later based on test requirements
    for specific boards.
    
    Signed-off-by: Adrian Ratiu <adrian.ratiu@collabora.com>

---
+++ config/rootfs/debos/scripts/install-firmware.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+FIRMWARE_SITE=https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+FIRMWARE_VERSION=d526e044bddaa2c2ad855c7296147e49be0ab03c
+
+DEST=${ROOTDIR}/lib/firmware
+mkdir -p $DEST
+
+# Because the linux-firmware repo is very big, we use the following
+# git init hack to fetch a single shallow commit to minimize time,
+# space and network bandwidth
+TMP_REPO=/tmp/linux-firmware
+mkdir -p $TMP_REPO && cd $TMP_REPO
+git init
+git remote add origin $FIRMWARE_SITE
+git fetch --depth 1 origin $FIRMWARE_VERSION
+git checkout FETCH_HEAD
+
+for fw_file in "$@"
+do
+    cp --parents "$fw_file" $DEST
+done
+
+########################################################################
+# Cleanup: remove downloaded firmware files
+########################################################################
+rm -rf $TMP_REPO
