905ce15ebe54a8125026111fad14faf2541ed1d0
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-11-22 13:47:06 +0000

    kernelci.lab: add Shell lab type
    Add the kernelci.lab.shell.Shell() class to generate a script from
    templates and run it as a process locally.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ kernelci/lab/shell.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+from jinja2 import Environment, FileSystemLoader
+from kernelci.lab import LabAPI
+
+
+class Shell(LabAPI):
+    BASE_PATH = 'config/scripts'
+
+    def generate(self, params, target, plan,
+                 callback_opts=None, base_path=None):
+        jinja2_env = Environment(loader=FileSystemLoader(
+            base_path or self.BASE_PATH
+        ))
+        template_path = plan.get_template_path(None)
+        template = jinja2_env.get_template(template_path)
+        return template.render(params)
+
+    def save_file(self, *args, **kwargs):
+        output_file = super().save_file(*args, **kwargs)
+        os.chmod(output_file, 0o775)
+        return output_file
+
+    def submit(self, job_path):
+        return subprocess.call(job_path)
+
+
+def get_api(lab, **kwargs):
+    """Get a Shell object"""
+    return Shell(lab, **kwargs)
