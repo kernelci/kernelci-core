80ced722bb75a9b48cbb9c0787b7eaf3b267340c
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-11-23 10:32:10 +0000

    kernelci.data: add missing json import
    Import missing json package which is needed for
    Database._print_http_error().  Also remove it from
    kernelci.data.kernelci_backend as it's not needed there any more.
    
    Fixes: 0618a1a3de47 ("kernelci.data: add Database._print_http_error()")
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/data/kernelci_backend.py
+++ kernelci/data/kernelci_backend.py
@@ -16,7 +16,6 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import json
 import requests
 import urllib
 from kernelci.data import Database
