975fe4ef87055fb66d2438dab07fb0a0c9b8e075
Michal Galka <michal.galka@collabora.com>
2021-12-01 21:48:23 +0100

    rootfs-configs.yaml: Add bullseye-cros-ec config
    Add Debian bullseye based image configuration for cros-ec tests.
    
    Signed-off-by: Michal Galka <michal.galka@collabora.com>

---
+++ config/rootfs/debos/scripts/bullseye-cros-ec-tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -x
+
+# Important: This script is run under QEMU
+
+set -e
+
+# Build-depends needed to build the test suites, they'll be removed later
+BUILD_DEPS="\
+    git \
+    python3-setuptools \
+    python3-pip
+"
+
+apt-get install --no-install-recommends -y ${BUILD_DEPS}
+apt-mark manual python3 libpython3-stdlib libpython3.9-stdlib python3.9
+
+########################################################################
+# Build tests                                                          #
+########################################################################
+
+BUILDFILE=/test_suites.json
+echo '{  "tests_suites": [' >> $BUILDFILE
+
+########################################################################
+# Build and install tests                                              #
+########################################################################
+
+CROS_URL="https://git.kernel.org/pub/scm/linux/kernel/git/chrome-platform/cros-ec-tests.git"
+CROS_SHA=$(git ls-remote ${CROS_URL} | head -n 1 | cut -f 1)
+
+pip3 install git+${CROS_URL}@${CROS_SHA}
+
+echo '    {"name": "cros-ec-tests", "git_url": "'$CROS_URL'", "git_commit": "'$CROS_SHA'" }' >> $BUILDFILE
+echo '  ]}' >> $BUILDFILE
+
+########################################################################
+# Cleanup: remove files and packages we don't want in the images       #
+########################################################################
+
+rm -fr /src/cros-ec-tests
+
+apt-get remove --purge -y ${BUILD_DEPS} perl-modules-5.32
+apt-get autoremove --purge -y
+apt-get clean
+
