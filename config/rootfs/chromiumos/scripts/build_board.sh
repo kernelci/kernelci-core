d16ac92bd3425a328ad93fb576981ee1b24cfb93
Denys Fedoryshchenko <denys.f@collabora.com>
2022-02-01 11:15:56 +0200

    CHROMEOS Add ChromiumOS image building functionality as rootfs option
    To build amd64-generic image (can run in qemu) we need:
    docker run -itd -v $(pwd):/kernelci-core --device /dev/kvm -v /dev:/dev --privileged kernelci/chromiumos
    ./kci_rootfs build --rootfs-config chromiumos-amd64-generic --data-path config/rootfs/chromiumos --arch amd64
    (And several hours of build time)
    
    qemu bootable image artifact will be generated at:
    config/rootfs/chromiumos/amd64-generic/chromiumos_test_image.bin
    (Must be used with USB storage disk in emulator, if we use libvirt/qemu)
    
    As ChromiumOS dont have strictly speaking "architecture", in our case it means ChromiumOS board name.
    So it can be amd64-generic, arm64-generic, hatch, etc.
    
    v2:
    - Pass all args, as for our build function we need args.board, while others need args.arch
    - Add default board option for now
    - Add --depth=1 option, as suggested, and tested
    - Add nproc in number of repo threads to scale better
    
    v3:
    - Make entry per board in rootfs-configs.yaml to improve backward compatibility
    - Add branch parameter
    - Delete build directory after generating artifacts
    
    v4:
    - Rebase on recent branch
    
    v5:
    - Remove unused comments
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- config/rootfs/chromiumos/scripts/build_board.sh
+++ config/rootfs/chromiumos/scripts/build_board.sh
@@ -50,3 +50,41 @@ gzip -1 "${DATA_DIR}/${BOARD}/chromiumos_test_image.bin"
 
 # Probably redundant, but better safe than sorry
 cleanup
+=======
+
+# Building SDK itself
+if [ ! -d chromiumos-sdk ]; then
+  sudo mkdir chromiumos-sdk
+  sudo chown user chromiumos-sdk
+  cd chromiumos-sdk
+  git config --global user.email "bot@kernelci.org"
+  git config --global user.name "KernelCI Bot"
+  git config --global color.ui false
+  repo init --repo-url https://chromium.googlesource.com/external/repo --manifest-url https://chromium.googlesource.com/chromiumos/manifest --manifest-name default.xml --manifest-branch ${BRANCH} --depth=1
+  repo sync -j$(nproc)
+  cros_sdk --create
+else
+  cd chromiumos-sdk
+  # We might have different branch
+  repo init --repo-url https://chromium.googlesource.com/external/repo --manifest-url https://chromium.googlesource.com/chromiumos/manifest --manifest-name default.xml --manifest-branch ${BRANCH} --depth=1
+  repo sync -j$(nproc)
+  cros_sdk --create
+fi
+
+# Compiling ChromiumOS image
+# Future possible option --profile=x, for example kernel-5_15, profiles are at /mnt/host/source/src/overlays/overlay-${BOARD}/profiles/
+cros_sdk setup_board --board=${BOARD} --force
+# Add serial support
+cros_sdk USE=pcserial ./build_packages --board=${BOARD}
+cros_sdk USE="tty_console_ttyS0" emerge-${BOARD} chromeos-base/tty
+cros_sdk ./build_image --enable_serial ttyS0 --board=${BOARD} --boot_args "earlyprintk=serial,keep console=tty0" --noenable_rootfs_verification test
+
+# Create artifacts dir and copy generated image and tast files
+sudo mkdir -p ${DATA_DIR}/${BOARD}
+sudo cp src/build/images/${BOARD}/latest/chromiumos_test_image.bin ${DATA_DIR}/${BOARD}
+sudo cp ./chroot/usr/bin/remote_test_runner ${DATA_DIR}/${BOARD}
+sudo cp ./chroot/usr/bin/tast ${DATA_DIR}/${BOARD}
+
+# Delete build directory
+[ -d "${DATA_DIR}/chromiumos-sdk/chroot/build" ] && sudo rm -rf ${DATA_DIR}/chromiumos-sdk/chroot/build && echo Build directory deleted
+>>>>>>> CHROMEOS Add ChromiumOS image building functionality as rootfs option
