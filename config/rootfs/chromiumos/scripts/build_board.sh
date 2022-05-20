8c6093a8b41b5351692841b909c98dd60b253951
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-05-20 13:34:49 +0100

    CHROMEOS rootfs/chromiumos: use serial parameter
    Use the "serial" config parameter in build_board.sh to choose the
    serial console device.
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- config/rootfs/chromiumos/scripts/build_board.sh
+++ config/rootfs/chromiumos/scripts/build_board.sh
@@ -2,6 +2,7 @@
 set -e
 BOARD=$1
 BRANCH=$2
+SERIAL=$3
 DATA_DIR=$(pwd)
 USERNAME=$(/usr/bin/id -run)
 
@@ -16,7 +17,7 @@ function cleanup()
 
 trap cleanup EXIT
 
-echo Preparing environment, branch ${BRANCH}
+echo "Preparing environment, branch ${BRANCH}"
 sudo mkdir chromiumos-sdk
 sudo chown user chromiumos-sdk
 cd chromiumos-sdk
@@ -28,29 +29,29 @@ repo sync -j$(nproc)
 echo Building SDK
 cros_sdk --create
 
-echo Board ${BOARD} setup
+echo "Board ${BOARD} setup"
 # Compiling ChromiumOS image
 # Future possible option --profile=x, for example kernel-5_15, profiles are at /mnt/host/source/src/overlays/overlay-${BOARD}/profiles/
 cros_sdk setup_board --board=${BOARD}
 
 # Without workarounds hatch and octopus build failing
 if [ "${BOARD}" == "hatch" ]; then
-echo Patching hatch specific issue
+echo "Patching hatch specific issue"
 sed -i 's/EC_BOARDS=()/EC_BOARDS=(hatch)/' src/third_party/chromiumos-overlay/eclass/cros-ec-board.eclass
 fi
 if [ "${BOARD}" == "octopus" ]; then
-echo Patching octopus specific issue
+echo "Patching octopus specific issue"
 sed -i s,'use fuzzer || die',"#use fuzzer || die", src/third_party/chromiumos-overlay/eclass/cros-ec-board.eclass
 fi
 
 # Add serial support
-echo Add serial support
+echo "Add serial ${SERIAL} support"
 cros_sdk USE=pcserial build_packages --board=${BOARD}
-cros_sdk USE="tty_console_ttyS0" emerge-"${BOARD}" chromeos-base/tty
-echo Building image
-cros_sdk ./build_image --enable_serial ttyS0 --board="${BOARD}" --boot_args "earlyprintk=serial,keep console=tty0" --noenable_rootfs_verification test
+cros_sdk USE="tty_console_${SERIAL}" emerge-"${BOARD}" chromeos-base/tty
+echo "Building image (${SERIAL})"
+cros_sdk ./build_image --enable_serial ${SERIAL} --board="${BOARD}" --boot_args "earlyprintk=serial,keep console=tty0" --noenable_rootfs_verification test
 
-echo Moving artifacts
+echo "Moving artifacts"
 # Create artifacts dir and copy generated image and tast files
 sudo mkdir -p "${DATA_DIR}/${BOARD}"
 sudo cp "src/build/images/${BOARD}/latest/chromiumos_test_image.bin" "${DATA_DIR}/${BOARD}"
