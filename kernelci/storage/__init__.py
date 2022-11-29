9f1c291c80f1e6afba24ad29f2525c3024b7eab8
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-11-29 14:25:38 +0100

    kernelci.storage: add base Storage interface class
    Add the base kernelci.storage.Storage class to provide an abstraction
    for storage solutions with concrete implementations.  It includes some
    basic interface methods upload_single() and upload_multiple() to
    upload either one or serveral files and return the URLs where they can
    be found.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/storage/__init__.py
+++ kernelci/storage/__init__.py
@@ -16,6 +16,7 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import importlib
 import os
 import requests
 from urllib.parse import urljoin
@@ -61,3 +62,72 @@ def upload_files(api, token, path, input_files):
     url = urljoin(api, 'upload')
     resp = requests.post(url, headers=headers, data=data, files=files)
     resp.raise_for_status()
+
+
+class Storage:
+    """Storage abstraction interface class"""
+
+    def __init__(self, config, credentials):
+        """Base class for interacting with a storage implementation
+
+        *config* is the storage configuration object loaded from YAML
+        *credentials* is an arbitrary object with credentials for the storage
+        """
+        self._config = config
+        self._credentials = credentials
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def credentials(self):
+        return self._credentials
+
+    def _upload(self, file_path, dest_path):
+        """Implementation method to upload files"""
+        raise NotImplementedError("_upload() needs to be implemented")
+
+    def upload_single(self, file_path, dest_path=''):
+        """Upload a single file to storage
+
+        Upload the file located in *file_path* to the storage at the
+        destination specified by *dest_path*.  Path elements from *file_path*
+        are not included in the destination path.  The returned value is the
+        full public URL that can be used to retrieve the file again.
+        """
+        self._upload([file_path], dest_path)
+        return urljoin(
+            self.config.base_url,
+            '/'.join([dest_path, os.path.basename(file_path)])
+        )
+
+    def upload_multiple(self, file_paths, dest_path=''):
+        """Upload multiple files to storage
+
+        Upload all the files listed in *file_paths* to storage at the
+        destination speciified by *dest_path*.  The path elements in the file
+        paths are not included in the destination paths.  The returned value is
+        a list with the public URL to retrieve each file matching the input
+        list in *file_paths*.
+        """
+        self._upload(file_paths, dest_path)
+        return [
+            urljoin(
+                self.config.base_url,
+                '/'.join([dest_path, os.path.basename(file_path)])
+            )
+            for file_path in file_paths
+        ]
+
+
+def get_storage(config, credentials):
+    """Get a Storage instance for a given storage configuration
+
+    Create and return a Storage implementation object instance that matches the
+    storage configuration in *config* using the provided *credentials*.
+    """
+    m = importlib.import_module('.'.join([
+        'kernelci', 'storage', config.storage_type
+    ]))
+    return m.get_storage(config, credentials)
