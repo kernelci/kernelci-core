32fbc94f73a7ac9205980e77ce53e1321ef54b28
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-12-17 08:57:30 +0100

    kernelci.cli: add commands registration
    Add a _COMMANDS dictionary to contain the sub-commands and functions
    to list them and call them with their names.  This is in preparation
    for adding sub-commands in the kernelci.cli package.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/cli/__init__.py
+++ kernelci/cli/__init__.py
@@ -6,3 +6,23 @@
 import sys
 
 from .base import Args, Command, parse_opts
+
+_COMMANDS = {
+}
+
+
+def list_command_names():
+    """Return a list with the command names"""
+    return list(_COMMANDS.keys())
+
+
+def call(name, args=None):
+    """Call a command registered with kernelci.cli
+
+    Call the command with the given *name* and optional arguments *args*.
+    """
+    cmd = _COMMANDS.get(name)
+    if not cmd:
+        print("Unknown command: {name}", file=sys.stderr, flush=True)
+        sys.exit(1)
+    _COMMANDS[name](args)
