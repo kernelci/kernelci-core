e862657bc4a9a95ecb85144b2f9c9d7d98fabe53
Denys Fedoryshchenko <denys.f@collabora.com>
2022-12-22 21:02:09 +0200

    install-bootrr.sh: Update bootrr to latest commit
    As tested on staging, bootrr need to be updated to latest commit
    Because it was forgotten on last update and LAVA template is changed,
    this is especially important to do before next rootfs update
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- config/rootfs/debos/scripts/install-bootrr.sh
+++ config/rootfs/debos/scripts/install-bootrr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BOOTRR_SITE=https://github.com/kernelci/bootrr.git
-BOOTRR_VERSION=e0a316de59153ecd4373b14063b0280505ce6e2e
+BOOTRR_VERSION=94dbf0b19d6e01489ce71c98cd3613b63c15f661
 
 BUILD_DEPS="\
     build-essential \
