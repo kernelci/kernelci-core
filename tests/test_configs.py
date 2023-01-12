4f1faa8bd22b8b04085aa0809a1cc80fff8238b8
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-01-12 13:53:21 +0100

    tests: add ConfigTest class based on unittest.TestCase
    Rework the config tests into a ConfigTest based a unittest.TestCase.
    This allows adding private class methos such as _load_config() to
    factor out the code to load the configuration both from plain YAML and
    as objects from kernelci.config.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -18,6 +18,7 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import unittest
 import yaml
 
 import kernelci.config
@@ -59,36 +60,39 @@ def test_architecture_init_name_only():
     assert architecture._filters == []  # filters does not have a property..
 
 
-def test_trees():
-    # ToDo: use relative path to test module 'configs/trees.yaml'
-    yaml_file_path = 'tests/configs/trees.yaml'
-    with open(yaml_file_path) as yaml_file:
-        ref_data = yaml.safe_load(yaml_file)
-    tree_names = ['kselftest', 'mainline', 'next']
-    assert all(name in ref_data['trees'] for name in tree_names)
-    config = kernelci.config.load(yaml_file_path)
-    trees_dump = yaml.dump(config['trees'])
-    trees_config = yaml.safe_load(trees_dump)
-    assert all(name in trees_config for name in tree_names)
-    assert ref_data['trees'] == trees_config
-    assert (
-        trees_config['next']['url'] ==
-        'https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git'
-    )
+class ConfigTest(unittest.TestCase):
 
+    def _load_config(self, yaml_file_path):
+        with open(yaml_file_path) as yaml_file:
+            ref_data = yaml.safe_load(yaml_file)
+        config = kernelci.config.load(yaml_file_path)
+        return ref_data, config
 
-def test_file_system_types():
-    yaml_file_path = 'tests/configs/file-system-types.yaml'
-    with open(yaml_file_path) as yaml_file:
-        ref_data = yaml.safe_load(yaml_file)
-    fs_names = ['buildroot', 'debian']
-    assert all(name in ref_data['file_system_types'] for name in fs_names)
-    config = kernelci.config.load(yaml_file_path)
-    fs_dump = yaml.dump(config['file_system_types'])
-    fs_config = yaml.safe_load(fs_dump)
-    assert all(name in fs_config for name in fs_names)
-    assert ref_data['file_system_types'] == fs_config
-    assert (
-        fs_config['debian']['url'] ==
-        'http://storage.kernelci.org/images/rootfs/debian'
-    )
+    def test_trees(self):
+        # ToDo: use relative path to test module 'configs/trees.yaml'
+        ref_data, config = self._load_config('tests/configs/trees.yaml')
+        tree_names = ['kselftest', 'mainline', 'next']
+        assert all(name in ref_data['trees'] for name in tree_names)
+        trees_dump = yaml.dump(config['trees'])
+        trees_config = yaml.safe_load(trees_dump)
+        assert all(name in trees_config for name in tree_names)
+        assert ref_data['trees'] == trees_config
+        assert (
+            trees_config['next']['url'] ==
+            'https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git'  # noqa
+        )
+
+    def test_file_system_types(self):
+        ref_data, config = self._load_config(
+            'tests/configs/file-system-types.yaml'
+        )
+        fs_names = ['buildroot', 'debian']
+        assert all(name in ref_data['file_system_types'] for name in fs_names)
+        fs_dump = yaml.dump(config['file_system_types'])
+        fs_config = yaml.safe_load(fs_dump)
+        assert all(name in fs_config for name in fs_names)
+        assert ref_data['file_system_types'] == fs_config
+        assert (
+            fs_config['debian']['url'] ==
+            'http://storage.kernelci.org/images/rootfs/debian'
+        )
