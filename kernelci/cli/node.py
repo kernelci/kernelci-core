fc6d9539dfae4579a2802f6fcbf9a370f154e806
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-01-17 16:38:05 +0100

    WIP kernelci.cli.node: add find

--- kernelci/cli/node.py
+++ kernelci/cli/node.py
@@ -27,6 +27,40 @@ class cmd_get(Command):  # pylint: disable=invalid-name
         return True
 
 
+class cmd_find(Command):  # pylint: disable=invalid-name
+    """Find nodes with arbitrary parameters"""
+    args = [
+        Args.api_config, Args.api_token,
+        {
+            'name': 'params',
+            'nargs': '+',
+            'help': "Parameters to find nodes in param=value format",
+        },
+    ]
+    opt_args = [
+        {
+            'name': '--limit',
+            'type': int,
+            'help': "Maximum number of nodes to retrieve",
+        },
+        {
+            'name': '--offset',
+            'type': int,
+            'help': "Offset when paginating results with a number of nodes",
+        },
+    ]
+
+    def __call__(self, configs, args):
+        config = configs['api_configs'][args.api_config]
+        api = kernelci_api.KernelCI_API(config, args.api_token)
+        params = dict(
+            tuple(param.split('=')) for param in args.params
+        )
+        nodes = api.get_nodes(params, args.offset, args.limit)
+        print(nodes)
+        return True
+
+
 def main(args=None):
     """Entry point for the command line tool"""
     sub_main("node", globals(), args)
