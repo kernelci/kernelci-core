9bdef2b72231d8777b75a77ed00e7f4d30a27817
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-01-17 12:56:46 +0100

    kernelci.storage: fix all pylint warnings
    Fix all pylint warnings in the kernelci.storage package.  Adjust the
    storage class names to match the convention for valid names.  These
    are not used outside the module as the objects are constructed via the
    the get_storage() module method.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/storage/__init__.py
+++ kernelci/storage/__init__.py
@@ -1,20 +1,10 @@
-# Copyright (C) 2020, 2021, 2022 Collabora Limited
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2020, 2021, 2022, 2023 Collabora Limited
 # Author: Lakshmipathi Ganapathi <lakshmipathi.ganapathi@collabora.com>
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
-#
-# This module is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 2.1 of the License, or (at your option)
-# any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""KernelCI storage abstraction package"""
 
 import importlib
 import os
@@ -35,14 +25,20 @@ class Storage:
 
     @property
     def config(self):
+        """Configuration object loaded from YAML"""
         return self._config
 
     @property
     def credentials(self):
+        """Credentials data"""
         return self._credentials
 
-    def _upload(self, file_path, dest_path):
-        """Implementation method to upload files"""
+    def _upload(self, file_paths, dest_path):
+        """Implementation method to upload files
+
+        The *file_paths* and *dest_path* arguments are of the same form as
+        passed to .upload_multiple().
+        """
         raise NotImplementedError("_upload() needs to be implemented")
 
     def upload_single(self, file_path, dest_path=''):
@@ -97,7 +93,7 @@ def get_storage(config, credentials):
     Create and return a Storage implementation object instance that matches the
     storage configuration in *config* using the provided *credentials*.
     """
-    m = importlib.import_module('.'.join([
+    module = importlib.import_module('.'.join([
         'kernelci', 'storage', config.storage_type
     ]))
-    return m.get_storage(config, credentials)
+    return module.get_storage(config, credentials)
