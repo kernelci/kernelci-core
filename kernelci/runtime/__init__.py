5f0431568d016db7d0ad282ea7b45f299dad40a0
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-03-06 11:10:52 +0100

    kernelci.runtime: fix all pylint errors in __init__.py
    Fix all pylint errors in kernelci/runtime/__init__.py.  Among other
    things, this removes the ._connect() method as it's not actually used.
    Some Runtime implementations can have something specific to them as
    needed.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/runtime/__init__.py
+++ kernelci/runtime/__init__.py
@@ -3,6 +3,8 @@
 # Copyright (C) 2019, 2021-2023 Collabora Limited
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 
+"""KernelCI Runtime environment base class definition"""
+
 import abc
 import importlib
 import json
@@ -12,10 +14,16 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 
-def add_kci_raise(env):
+def add_kci_raise(jinja2_env):
+    """Add a kci_raise function to use in templates
+
+    This adds a `kci_raise` function to a given Jinja2 environment `jinja2_env`
+    so it can be used to raise exceptions from template files when for example
+    some template parameters are not valid.
+    """
     def template_exception(msg):
         raise Exception(msg)
-    env.globals['kci_raise'] = template_exception
+    jinja2_env.globals['kci_raise'] = template_exception
 
 
 class Runtime(abc.ABC):
@@ -23,35 +31,35 @@ class Runtime(abc.ABC):
 
     TEMPLATES = ['config/runtime', '/etc/kernelci/runtime']
 
-    def __init__(self, config, **kwargs):
+    # pylint: disable=unused-argument
+    def __init__(self, config, user=None, token=None):
         """A Runtime object can be used to run jobs in a runtime environment
 
         *config* is a kernelci.config.runtime.Runtime object
         """
         self._config = config
-        self._server = self._connect(**kwargs)
         self._devices = None
         self._templates = self.TEMPLATES
 
     @property
     def config(self):
+        """RuntimeConfig object for this Runtime instance"""
         return self._config
 
     @property
     def devices(self):
+        """List of devices available in this runtime"""
         if self._devices is None:
             self._devices = self._get_devices()
         return self._devices
 
     @property
     def templates(self):
+        """List of template directories used with this runtime"""
         return self._templates
 
-    def _get_devices(self):
-        return list()
-
-    def _connect(self, *args, **kwargs):
-        return None
+    def _get_devices(self):  # pylint: disable=no-self-use
+        return []
 
     def _get_template(self, plan_config):
         jinja2_env = Environment(loader=FileSystemLoader(self.templates))
@@ -68,6 +76,7 @@ class Runtime(abc.ABC):
         """
         self._devices = data
 
+    # pylint: disable=unused-argument disable=no-self-use
     def device_type_online(self, device_type_config):
         """Check whether a given device type is online
 
@@ -111,7 +120,7 @@ class Runtime(abc.ABC):
              plan
         """
 
-    def save_file(self, job, output_path, params):
+    def save_file(self, job, output_path, params, encoding='utf-8'):
         """Save a test job definition in a file.
 
         *job* is the job definition data
@@ -122,7 +131,7 @@ class Runtime(abc.ABC):
         """
         file_name = self.job_file_name(params)
         output_file = os.path.join(output_path, file_name)
-        with open(output_file, 'w') as output:
+        with open(output_file, 'w', encoding=encoding) as output:
             output.write(job)
         return output_file
 
@@ -143,7 +152,7 @@ def get_runtime(config, user=None, token=None, runtime_json=None):
     runtime_module = importlib.import_module(module_name)
     runtime = runtime_module.get_runtime(config, user=user, token=token)
     if runtime_json:
-        with open(runtime_json) as json_file:
+        with open(runtime_json, encoding='utf-8') as json_file:
             devices = json.load(json_file)['devices']
             runtime.import_devices(devices)
     return runtime
