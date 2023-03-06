7d5cf181ff820dc8bdee74e7d05d7cdd5909b8ce
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-03-06 11:26:08 +0100

    kernelci.runtime.legacy: disable all pylint checks
    Disable all the pylint checks in kernelci.runtime.legacy as this is
    just a temporary measure to load code from kernelci.legacy.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/runtime/legacy/lava_rest.py
+++ kernelci/runtime/legacy/lava_rest.py
@@ -3,4 +3,6 @@
 # Copyright (C) 2023 Collabora Limited
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 
+# pylint: disable=all
+
 from kernelci.legacy.lava.lava_rest import get_runtime
