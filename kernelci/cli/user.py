807cd30b4efc6515c4a9e83ebf4b713314d6f05c
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-03-09 14:48:37 +0100

    kernelci.cli.user: add get_token command
    Add a "kci user get_token" command to create a new API token.  This
    requires the user to input a password interactively on the command
    line.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/cli/user.py
+++ kernelci/cli/user.py
@@ -5,6 +5,8 @@
 
 """Tool to manage KernelCI API users"""
 
+import getpass
+
 from .base import Args, APICommand, sub_main
 
 
@@ -17,6 +19,25 @@ class cmd_whoami(APICommand):  # pylint: disable=invalid-name
         api = self._get_api(configs, args)
         data = api.whoami()
         self._print_json(data, args.indent)
+        return True
+
+
+class cmd_get_token(APICommand):  # pylint: disable=invalid-name
+    """Create a new API token for the current user"""
+    args = APICommand.args + [Args.username]
+    opt_args = APICommand.opt_args + [
+        {
+            'name': '--scopes',
+            'action': 'append',
+            'help': "Security scopes",
+        },
+    ]
+
+    def __call__(self, configs, args):
+        password = getpass.getpass()
+        api = self._get_api(configs, args)
+        token = api.create_token(args.username, password, args.scopes)
+        self._print_json(token, args.indent)
         return True
 
 
