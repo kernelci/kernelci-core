db426ae550c953d936f3060fba5006c8b649c740
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-04-25 18:01:07 +0100

    kernelci.runtime: lava: add initial new LAVA runtime
    Add an initial Runtime implementation based on the new class which
    only supports the LAVA REST API v0.2 and has the bare minimum needed
    to generate and submit jobs.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/runtime/lava/__init__.py
+++ kernelci/runtime/lava/__init__.py
@@ -9,6 +9,11 @@
 
 # pylint: disable=all
 
+from collections import namedtuple
+import time
+import requests
+import yaml
+from urllib.parse import urljoin
 from jinja2 import Environment, FileSystemLoader
 import os
 from kernelci.runtime import add_kci_raise, Runtime
@@ -80,3 +85,75 @@ class LavaRuntime(Runtime):
             'callback_dataset': opts['dataset'],
             'callback_type': callback_type,
         })
+
+
+class LAVA(Runtime):
+    """Runtime implementation to run jobs in a LAVA lab
+
+    LAVA is a framework for running tests on real hardware or QEMU.  This
+    provides the basic features needed to generate a job defintion, submit it
+    and wait for the job to complete.
+
+    This currently only supports the REST API v0.2 and doesn't look for online
+    devices or aliases.  It also doesn't take into account the callback
+    parameters so results can't be sent to the API yet.  It requiers an API
+    token to be provided to the constructor.  The user name is not used.
+    """
+    API_VERSION = 'v0.2'
+    RestAPIServer = namedtuple('RestAPIServer', ['url', 'session'])
+
+    def __init__(self, configs, **kwargs):
+        super().__init__(configs, **kwargs)
+        self._server = self._connect()
+
+    def generate(self, job, params):
+        template = self._get_template(job.config)
+        return template.render(params)
+
+    def submit(self, job_path):
+        with open(job_path, 'r', encoding='utf-8') as job_file:
+            job = job_file.read()
+            job_id = self._submit(job)
+            return job_id
+
+    def get_job_id(self, job_object):
+        job_id = int(job_object)
+        return job_id
+
+    def wait(self, job_object):
+        job_id = int(job_object)
+        job_url = urljoin(self._server.url, '/'.join(['jobs', str(job_id)]))
+        while True:
+            resp = self._server.session.get(job_url)
+            resp.raise_for_status()
+            data = resp.json()
+            if data['state'] == 'Finished':
+                health = data['health']
+                return 0 if health == 'Complete' else 1
+            time.sleep(3)
+
+    def _connect(self):
+        rest_url = f'{self.config.url}/api/{self.API_VERSION}/'
+        rest_api = self.RestAPIServer(rest_url, requests.Session())
+        rest_api.session.params = {'format': 'json', 'limit': '256'}
+        rest_api.session.headers = {
+            'authorization': f'Token {self._token}',
+            'content-type': 'application/json',
+        }
+        return rest_api
+
+    def _submit(self, job):
+        jobs_url = urljoin(self._server.url, 'jobs/')
+        job_data = {
+            'definition': yaml.dump(yaml.load(job, Loader=yaml.CLoader)),
+        }
+        resp = self._server.session.post(
+            jobs_url, json=job_data, allow_redirects=False
+        )
+        resp.raise_for_status()
+        return resp.json()['job_ids'][0]
+
+
+def get_runtime(runtime_config, **kwargs):
+    """Get a LAVA runtime object"""
+    return LAVA(runtime_config, **kwargs)
