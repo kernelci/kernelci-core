cf416f060d3984b85d15ceb527e15651951a0e07
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-06-22 20:55:31 +0200

    kernelci.config: move event condition to scheduler
    Remove the Job.run_on list of conditions and replace it with a single
    event attribute in the scheduler entries since we can already define
    multiple entries for a same job.  This is as discussed on the mailing
    list and GitHub.
    
    Update unit tests accordingly.
    
    Link: https://lore.kernel.org/r/09c2d79d-27f3-1cb1-0d52-df003926a132@collabora.com
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/config/job.py
+++ kernelci/config/job.py
@@ -14,12 +14,10 @@ class Job(YAMLConfigObject):
     yaml_tag = '!Job'
 
     # pylint: disable=too-many-arguments
-    def __init__(self, name, template, image=None,
-                 run_on=None, params=None):
+    def __init__(self, name, template, image=None, params=None):
         self._name = name
         self._template = template
         self._image = image
-        self._run_on = run_on or []
         self._params = params or {}
 
     @property
@@ -38,11 +36,6 @@ class Job(YAMLConfigObject):
         return self._image
 
     @property
-    def run_on(self):
-        """Criteria for the job to be run"""
-        return list(dict(crit) for crit in self._run_on)
-
-    @property
     def params(self):
         """Arbitrary parameters passed to the template"""
         return dict(self._params)
@@ -50,7 +43,7 @@ class Job(YAMLConfigObject):
     @classmethod
     def _get_yaml_attributes(cls):
         attrs = super()._get_yaml_attributes()
-        attrs.update({'template', 'image', 'run_on', 'params'})
+        attrs.update({'template', 'image', 'params'})
         return attrs
 
 
