kernelci/kernelci-core

commit a425ea5e81e3a2fffdfdfb226b5385547f081b2f

docker.py: Return non-zero exit code on build failure
If Docker image build failed, we should return non-zero,
so deploy scripts are aware about failure.
Also print some debug information about failure.

Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

kernelci/cli/docker.py
@@ -120,13 +120,10 @@ class cmd_build(DockerBuildGenerateCommand):  # pylint: disable=invalid-name
     def _build_image(cls, dockerfile, tag, buildargs, nocache):
         client = docker.from_env()
         dockerfile_obj = io.BytesIO(dockerfile.encode())
-        try:
-            return client.images.build(
-                fileobj=dockerfile_obj, tag=tag, buildargs=buildargs,
-                nocache=nocache,
-            )
-        except docker.errors.BuildError as exc:
-            return exc.msg, exc.build_log
+        return client.images.build(
+            fileobj=dockerfile_obj, tag=tag, buildargs=buildargs,
+            nocache=nocache,
+        )
 
     @classmethod
     def _dump_dockerfile(cls, dockerfile):
@@ -170,9 +167,15 @@ class cmd_build(DockerBuildGenerateCommand):  # pylint: disable=invalid-name
         buildargs = dict(
             barg.split('=') for barg in args.build_arg
         ) if args.build_arg else {}
-        _, build_log = self._build_image(
-            dockerfile, name, buildargs, args.no_cache
-        )
+        try:
+            _, build_log = self._build_image(
+                dockerfile, name, buildargs, args.no_cache
+            )
+        except docker.errors.BuildError as exc:
+            if args.verbose:
+                self._dump_log(exc.build_log)
+            print("Build failed, exception: "+str(exc.msg))
+            return False
         if args.verbose:
             self._dump_log(build_log)
         if args.push:

commit 21c68174b7d721919ee8893410784809d9c1b7a9

kernelci.cli: docker: clean up docker build error propagation
Clean up the implementation of how docker build errors are being
propagated by removing duplicated logic and printing the exception
message on stderr.

Fixes: a425ea5e81e3 ("docker.py: Return non-zero exit code on build failure")
Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

kernelci/cli/docker.py
@@ -9,6 +9,7 @@
 
 import io
 import json
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 import docker
@@ -171,13 +172,15 @@ class cmd_build(DockerBuildGenerateCommand):  # pylint: disable=invalid-name
             _, build_log = self._build_image(
                 dockerfile, name, buildargs, args.no_cache
             )
+            build_status = True
         except docker.errors.BuildError as exc:
-            if args.verbose:
-                self._dump_log(exc.build_log)
-            print("Build failed, exception: "+str(exc.msg))
-            return False
+            print(exc.msg, file=sys.stderr)
+            build_log = exc.build_log
+            build_status = False
         if args.verbose:
             self._dump_log(build_log)
+        if build_status is not True:
+            return build_status
         if args.push:
             print(f"Pushing {name} to Docker Hub")
             push_log = self._push_image(base_name, tag_name)