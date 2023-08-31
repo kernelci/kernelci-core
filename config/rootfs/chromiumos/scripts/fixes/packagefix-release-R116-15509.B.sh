99f32043d49b10c18534c8745dbfa3125917a421
Denys Fedoryshchenko <denys.f@collabora.com>
2023-08-31 16:45:36 +0300

    CHROMEOS packagefix-release-R116-15509.B.sh: Add tpm workarounds
    During the testing we noticed that some boards still need TPM
    flag to be enabled manually, to work properly.
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- config/rootfs/chromiumos/scripts/fixes/packagefix-release-R116-15509.B.sh
+++ config/rootfs/chromiumos/scripts/fixes/packagefix-release-R116-15509.B.sh
@@ -1,1 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+echo "packagefix: Applying R116 specific workarounds"
+
+case ${BOARD} in
+    dedede)
+    grep -q "tpm2" src/overlays/baseboard-dedede/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2 cr50_onboard"' >>src/overlays/baseboard-dedede/profiles/base/make.defaults
+    ;;
+    hatch)
+    grep -q "tpm2" src/overlays/baseboard-hatch/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/baseboard-hatch/profiles/base/make.defaults
+    ;;
+    volteer)
+    grep -q "tpm2" src/overlays/baseboard-volteer/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/baseboard-volteer/profiles/base/make.defaults
+    ;;
+    zork)
+    grep -q "tpm2" src/overlays/overlay-zork/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/overlay-zork/profiles/base/make.defaults
+    ;;
+    grunt)
+    grep -q "tpm2" src/overlays/baseboard-grunt/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/baseboard-grunt/profiles/base/make.defaults
+    ;;
+    *)
+    echo "No workarounds found for this board"
+    ;;
+esac
