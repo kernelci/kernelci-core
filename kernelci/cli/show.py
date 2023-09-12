3126914cee4af11e18198a3232a5fc0fed4cee71
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-09-12 09:46:43 +0200

    kernelci.cli: show: replace full path with node name
    Rather than showing the full path for the node, which can grow very
    long and is repeating the parent nodes' names already in the output,
    only show the node name.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/cli/show.py
+++ kernelci/cli/show.py
@@ -64,12 +64,12 @@ class cmd_results(APICommand):  # pylint: disable=invalid-name
             print(fmt.format(key=self._color(key, 'blue'), value=value))
 
     def _dump_results(self, api, node, indent=0, max_depth=0):
-        fmt = f"{{space}}{{path:{64-indent*2}s}}{{result:6}}{{node_id}}"
+        fmt = f"{{space}}{{name:{64-indent*2}s}}{{result:6}}{{node_id}}"
         node_id = node['id']
         result = node['result'] or '----'
         line = fmt.format(
             space='  '*indent,
-            path='.'.join(node['path']),
+            name=node['name'],
             result=result,
             node_id=node_id,
         )
