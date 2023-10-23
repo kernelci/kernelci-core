6fdf1158ba323912d3e574ea7bcd87d1da53cc73
Adrian Ratiu <adrian.ratiu@collabora.com>
2023-10-23 20:32:29 +0300

    CHROMEOS: rootfs: add and enable R118 manifest
    The src/scripts/build_image was removed in favor of one installed
    in $PATH, so we need to replace the ./build_image invocation with
    a plain build_image.
    
    minigbm:
     - Dropped mediatek backend in favor of drv_dumb backend, no more
       divergence for the platform/minigbm component.
    
    chromiumos-overlay:
     - Reworked the minigbm patch into a single two liner patch to
       activate drv_dumb
     - Droppped the chromeos-kernel-5_10 bump in favor of the 5.10
       shipped in R118
     - Fixed broken portage manifest for media-libs/cros-camera-libfs
     - Masked newer chromeos-chrome ebuild version which doesn't have
       a binpkg, to avoid unnecessary long build times.
     - Bumped chromeos-installer ebuild as a result of forking the
       platform2 overlay.
    
    platform2:
     - Forked to disable failing installer tpm check, until a fix is
     landed for b:291036452.
    
    graphics:
     - Backported patch from R119 to avoid a conflict, still need to
       keep this patch in R118
    
    board-overlays:
     - Forward ported the last remaining commit without conflicts.
       Will keep it for now but gradually will reduce its footprint
       as MTK SoCs start using the newer Google kernels with 6.6
     - Bumped octopus to 5.15 because tast fails with 4.14.
    
    kernel v5.10:
     - Removed fork in favor of cros-kernel 5.10
    
    packagefix-release-R118-15604.B.sh:
     - Dropped some of the tpm2 fixes which are not necessary anymore
     - More of these fixes landed post R118 and some are still pending
       for gurnt, zork and hatch (see CL 4868824).
    
    Signed-off-by: Adrian Ratiu <adrian.ratiu@collabora.com>

---
+++ config/rootfs/chromiumos/scripts/fixes/packagefix-release-R118-15604.B.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "packagefix: Applying R118 specific workarounds"
+
+# TODO(aratiu): Drop these workarounds once CL 4868824 lands
+# CL 4868824 only adds for grunt/zork/hatch because the others have been added post R118
+case ${BOARD} in
+    hatch)
+    grep -q "tpm2" src/overlays/baseboard-hatch/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/baseboard-hatch/profiles/base/make.defaults
+    ;;
+    zork)
+    grep -q "tpm2" src/overlays/overlay-zork/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/overlay-zork/profiles/base/make.defaults
+    ;;
+    grunt)
+    grep -q "tpm2" src/overlays/baseboard-grunt/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2 cr50_onboard"' >>src/overlays/baseboard-grunt/profiles/base/make.defaults
+    ;;
+    puff)
+    grep -q "tpm2" src/overlays/baseboard-puff/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/baseboard-puff/profiles/base/make.defaults
+    ;;
+    guybrush)
+    grep -q "tpm2" src/overlays/overlay-guybrush/profiles/base/make.defaults || echo 'USE="${USE} -tpm tpm2"' >>src/overlays/overlay-guybrush/profiles/base/make.defaults
+    ;;
+    *)
+    echo "No workarounds found for this board"
+    ;;
+esac
