af2cb384b155d097eece32a3835c720e5cef3ebb
Guillaume Tucker <gtucker@gtucker.io>
2023-12-07 10:32:59 +0100

    kernelci.cli.user: use API.User
    Update the kernelci.cli.user command to use the new API.User abstract
    class bindings.
    
    Signed-off-by: Guillaume Tucker <gtucker@gtucker.io>

--- kernelci/cli/user.py
+++ kernelci/cli/user.py
@@ -35,7 +35,7 @@ def kci_user():
 def whoami(config, api, indent, secrets):
     """Get the current user's details with API authentication"""
     api = get_api(config, api, secrets)
-    data = api.whoami()
+    data = api.user.whoami()
     echo_json(data, indent)
 
 
@@ -49,7 +49,7 @@ def find(attributes, config, api, indent, secrets):
     """Find user profiles with arbitrary attributes"""
     api = get_api(config, api, secrets)
     attributes = split_attributes(attributes)
-    users = api.get_users(attributes)
+    users = api.user.find(attributes)
     data = json.dumps(users, indent=indent or None)
     echo = click.echo_via_pager if len(users) > 1 else click.echo
     echo(data)
@@ -64,7 +64,7 @@ def token(username, config, api):
     """Create a new API token using a user name and password"""
     api = get_api(config, api)
     password = getpass.getpass()
-    api_token = api.create_token(username, password)
+    api_token = api.user.create_token(username, password)
     click.echo(api_token['access_token'])
 
 
@@ -83,13 +83,13 @@ def update(attributes, username, config, api, secrets):
 
     api = get_api(config, api, secrets)
     if username:
-        users = api.get_users({"username": username})
+        users = api.user.find({"username": username})
         if not users:
             raise click.ClickException(f"User not found: {username}")
         user_id = users[0]['id']
     else:
         user_id = None
-    api.update_user(fields, user_id)
+    api.user.update(fields, user_id)
 
 
 @kci_user.command(secrets=True)
@@ -110,7 +110,7 @@ def add(username, email, config, api, secrets):
         'password': password,
     }
     api = get_api(config, api, secrets)
-    api.create_user(user)
+    api.user.add(user)
 
 
 @kci_user.command
@@ -121,15 +121,15 @@ def add(username, email, config, api, secrets):
 def verify(username, config, api):
     """Verify the user's email address"""
     api = get_api(config, api)
-    users = api.get_users({"username": username})
+    users = api.user.find({"username": username})
     if not users:
         raise click.ClickException(f"User not found: {username}")
     user = users[0]
     email = user['email']
     click.echo(f"Sending verification token to {email}")
-    api.request_verification_token(email)
+    api.user.request_verification_token(email)
     verification_token = click.prompt("Verification token")
-    api.verify_user(verification_token)
+    api.user.verify_email(verification_token)
     click.echo("Email verification successful!")
 
 
@@ -142,7 +142,7 @@ def verify(username, config, api):
 def get(user_id, config, api, indent, secrets):
     """Get a user with a given ID"""
     api = get_api(config, api, secrets)
-    user = api.get_user(user_id)
+    user = api.user.get(user_id)
     echo_json(user, indent)
 
 
@@ -154,11 +154,11 @@ def get(user_id, config, api, indent, secrets):
 def activate(username, config, api, secrets):
     """Activate user account (admin only)"""
     api = get_api(config, api, secrets)
-    users = api.get_users({"username": username})
+    users = api.user.find({"username": username})
     if not users:
         raise click.ClickException(f"User not found: {username}")
     user = users[0]
-    api.update_user({"is_active": 1}, user['id'])
+    api.user.update({"is_active": 1}, user['id'])
 
 
 @kci_user.command(secrets=True)
@@ -169,11 +169,11 @@ def activate(username, config, api, secrets):
 def deactivate(username, config, api, secrets):
     """Deactivate a user account (admin only)"""
     api = get_api(config, api, secrets)
-    users = api.get_users({"username": username})
+    users = api.user.find({"username": username})
     if not users:
         raise click.ClickException(f"User not found: {username}")
     user = users[0]
-    api.update_user({"is_active": 0}, user['id'])
+    api.user.update({"is_active": 0}, user['id'])
 
 
 @kci_user.group(name='password')
@@ -194,7 +194,7 @@ def password_update(username, config, api):
     retyped = getpass.getpass("Retype new password: ")
     if new_password != retyped:
         raise click.ClickException("Sorry, passwords do not match")
-    api.update_password(username, current_password, new_password)
+    api.user.update_password(username, current_password, new_password)
 
 
 @user_password.command(name='reset')
@@ -205,19 +205,19 @@ def password_update(username, config, api):
 def password_reset(username, config, api):
     """Reset password for a user account"""
     api = get_api(config, api)
-    users = api.get_users({"username": username})
+    users = api.user.find({"username": username})
     if not users:
         raise click.ClickException(f"User not found: {username}")
     user = users[0]
     email = user['email']
     click.echo(f"Sending reset token to {email}")
-    api.request_password_reset_token(email)
+    api.user.request_password_reset_token(email)
     reset_token = click.prompt("Reset token")
     password = getpass.getpass("New password: ")
     retyped = getpass.getpass("Retype new password: ")
     if password != retyped:
         raise click.ClickException("Sorry, passwords do not match")
-    api.reset_password(reset_token, password)
+    api.user.reset_password(reset_token, password)
 
 
 @kci_user.group(name='group')
@@ -261,14 +261,14 @@ def group_add(name, config, api, secrets):
 def join(name, username, config, api, secrets):
     """Add a user to a group (admin only)"""
     api = get_api(config, api, secrets)
-    users = api.get_users({"username": username})
+    users = api.user.find({"username": username})
     if not users:
         raise click.ClickException(f"User not found: {username}")
     user = users[0]
     groups = [name]
     for group in users[0]['groups']:
         groups.append(group['name'])
-    api.update_user({"groups": groups}, user['id'])
+    api.user.update({"groups": groups}, user['id'])
 
 
 @user_group.command(secrets=True)
@@ -279,13 +279,13 @@ def join(name, username, config, api, secrets):
 def leave(name, config, api, secrets):
     """Leave a user group"""
     api = get_api(config, api, secrets)
-    user = api.whoami()
+    user = api.user.whoami()
     groups = []
     for group in user['groups']:
         groups.append(group['name'])
     if name in groups:
         groups.remove(name)
-        api.update_user({"groups": groups}, None)
+        api.user.update({"groups": groups}, None)
 
 
 @user_group.command(secrets=True)
