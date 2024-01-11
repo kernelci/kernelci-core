bac54c988381db8740e63596168c83c26cbcb71c
Jeny Sadadia <jeny.sadadia@collabora.com>
2024-01-11 12:32:34 +0530

    Add pyproject.toml for python packaging
    With python 3.12 the support for setup.py is
    deprecated. Hance, update the packaging and
    distribution of 'kernelci' package by adding
    'pyproject.toml' file.
    Add all `kci_*` scripts to `kernelci/scripts`
    directory and add entrypoints in the TOML file.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ kernelci/scripts/kci.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2022-2024 Collabora Limited
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+"""KernelCI Command Line Tool entrypoint"""
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+
+from kernelci.cli import kci
+from kernelci.cli import (  # pylint: disable=unused-import
+    api as kci_api,
+    config as kci_config,
+    docker as kci_docker,
+    event as kci_event,
+    job as kci_job,
+    node as kci_node,
+    storage as kci_storage,
+    user as kci_user,
+)
+
+
+def main():
+    kci()  # pylint: disable=no-value-for-parameter
+
+
+if __name__ == '__main__':
+    main()
