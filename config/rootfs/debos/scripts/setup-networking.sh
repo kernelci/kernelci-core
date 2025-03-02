7b3f229bddfb2a1970fa172e340d704f5ed88e03
Denys Fedoryshchenko <denys.f@collabora.com>
2025-03-02 14:36:18 +0200

    (setup-networking.sh): Add cloudflare resolver
    We are observing very strange resolving issues:
    https://github.com/kernelci/kernelci-pipeline/issues/1064
    
    This is attempt to improve DNS reliability.
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- config/rootfs/debos/scripts/setup-networking.sh
+++ config/rootfs/debos/scripts/setup-networking.sh
@@ -18,5 +18,6 @@ fi
 ln -sf /lib/systemd/resolv.conf /etc/resolv.conf
 
 # Setup some static DNS defaults (Google Public)
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
