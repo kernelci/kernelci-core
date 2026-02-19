8c6dc4e1da72ef3784922a16811d22ad8192cd19
Denys Fedoryshchenko <denys.f@collabora.com>
2026-02-19 17:38:17 +0200

    rootfs: trixie-rt fails on armhf, try to make sure we build latest snapshot
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- config/rootfs/debos/scripts/trixie-rt.sh
+++ config/rootfs/debos/scripts/trixie-rt.sh
@@ -17,6 +17,7 @@ BUILD_DEPS="\
     m4 \
     libc6-dev \
     libnuma-dev \
+    python3 \
 "
 
 apt-get install --no-install-recommends -y  ${BUILD_DEPS}
@@ -29,7 +30,11 @@ mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
 git config --global http.sslverify false
 
 GIT_URL="https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git"
-GIT_SHA=$(git ls-remote ${GIT_URL} | head -n 1 | cut -f 1)
+# Current rt-tests commit used for deterministic builds (main branch HEAD at snapshot time).
+# Update the value below when you intentionally rebuild against a newer rt-tests snapshot.
+# To refresh:
+#   GIT_SHA=$(git ls-remote ${GIT_URL} refs/heads/main | cut -f 1)
+GIT_SHA=4e68b52f0e0c9777c91088948374c6ee3d4a1f6b
 
 echo '    {"name": "rt-tests", "git_url": "'$GIT_URL'", "git_commit": "'$GIT_SHA'" }' >> $BUILDFILE
 echo '  ]}' >> $BUILDFILE
